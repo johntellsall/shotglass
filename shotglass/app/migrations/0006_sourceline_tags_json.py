a9a723c07df1b3938a65c0db3cef47cdeb1eeb13
John Mitchell <john@johntellsall.com>
2015-09-11 22:33:34 -0700

    add tags; highlight alternate files/tags

---
+++ shotglass/app/migrations/0006_sourceline_tags_json.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('app', '0005_sourceline_length'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='sourceline',
+            name='tags_json',
+            field=models.CharField(max_length=1000, null=True, blank=True),
+        ),
+    ]
