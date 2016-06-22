6929d372d033e8330a7c1cf775da028a0988df44
John Mitchell <john@johntellsall.com>
2016-06-21 20:45:28 -0700

    store Python code complexity

---
+++ shotglass/app/migrations/0016_progradon.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('app', '0015_remove_sourceline_tags_json'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='ProgRadon',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('type', models.CharField(max_length=1)),
+                ('complexity', models.IntegerField()),
+                ('sourceline', models.OneToOneField(null=True, blank=True, to='app.SourceLine')),
+            ],
+        ),
+    ]
---
+++ shotglass/app/migrations/0017_auto_20160622_0339.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('app', '0016_progradon'),
+    ]
+
+    operations = [
+        migrations.RenameField(
+            model_name='progradon',
+            old_name='type',
+            new_name='kind',
+        ),
+    ]
