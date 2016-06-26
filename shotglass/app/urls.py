36b1211196cddef27067ba40d6bada38710b68e0
John Mitchell <john@johntellsall.com>
2016-06-26 14:45:13 -0700

    improve navigation by linking from the Project page to the List Projects page

--- shotglass/app/urls.py
+++ shotglass/app/urls.py
@@ -4,10 +4,12 @@ from django.conf.urls import url
 
 from . import views
 
+
 urlpatterns = [
-    url(r'^$', views.list_projects),
+    url(r'^$', views.list_projects, name='list_projects'),
     url(r'(?P<project>.+)/$', views.list_symbols, name='list_symbols'),
     url(r'(?P<project>.+)/render', views.render, name='render'),
     url(r'(?P<project>.+)/draw', views.draw, name='draw'),
-    url(r'(?P<project>.+)/index_symbols', views.index_symbols),
+    url(r'(?P<project>.+)/index_symbols', views.index_symbols,
+        name='index_symbols'),
 ]
