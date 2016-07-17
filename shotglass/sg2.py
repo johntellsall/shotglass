58021f81cd4117f369839b2bc36abedcb1466147
John Mitchell <john@johntellsall.com>
2016-07-16 18:51:26 -0700

    output number of commits for each minor change

--- shotglass/sg2.py
+++ shotglass/sg2.py
@@ -3,7 +3,14 @@ import os
 from git import Repo
 
 repo = Repo(os.path.expanduser('~/src/iproute2'))
+
 tags = [tag.name.lstrip('v') for tag in repo.tags
     if tag.name.startswith('v3.') or tag.name == 'v4.0.0']
-tags.sort(key=lambda st: map(int, st.split('.')))
-print tags
+tags.sort(key=lambda st: map(int, st.lstrip('v').split('.')))
+tags = ['v'+version for version in tags]
+
+old = tags.pop(0)
+for new in tags:
+    print '{:7} - {:7}: {} commits'.format(
+        old, new, len(repo.tags[old].commit.diff(new)))
+    old = new
