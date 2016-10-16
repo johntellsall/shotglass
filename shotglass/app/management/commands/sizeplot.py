5f45184134974fa6ac0a92e456f7b8ac0605a265
John Mitchell <john@johntellsall.com>
2016-10-15 18:53:32 -0700

    annotate version

--- shotglass/app/management/commands/sizeplot.py
+++ shotglass/app/management/commands/sizeplot.py
@@ -15,7 +15,9 @@ def natural_sort_key(s, _nsre=re.compile('([0-9]+)')):
 def get_stats(tag):
     symbols = models.SourceLine.objects.filter(project=tag)
     length = symbols.aggregate(Sum('length')).values()[0]
-    return dict(length=length)
+    num_symbols = symbols.count()
+    num_files = symbols.values_list('path', flat=True).distinct().count()
+    return dict(length=length, num_files=num_files, num_symbols=num_symbols)
 
 
 class Command(BaseCommand):
@@ -35,21 +37,42 @@ class Command(BaseCommand):
         tags = models.SourceLine.objects.filter(
             project__startswith='ansible-').values_list('project', flat=True).distinct()
         tags = sorted(tags, key=natural_sort_key)
+        if 0:
+            tags = tags[:10]
         print tags
 
-        tag_length = {}
-        tag_lengths = []
+        tag_stats = {}
         for tag in tags:
-            stats = get_stats(tag)
-            tag_length[tag] = stats['length']
-            tag_lengths.append(stats['length'])
-            print tag, stats['length']
+            tag_stats[tag] = get_stats(tag)
 
-        plt.plot(tag_lengths)
+        for index, tag in enumerate(tags):
+            m = re.search(r'v(1.0|2.0.0.0-1)$', tag)
+            if not m:
+                continue
+            version = m.group(1)
+            plt.annotate(
+                'v{}'.format(version[0]), xy=(index, 20), xytext=(index, 1.5))
+                    # arrowprops=dict(facecolor='black', shrink=0.05),
+                    # )
+
+        STYLES = dict(length='r-', num_symbols='b-', num_files='g-')
+
+        plt.style.use('fivethirtyeight') # see plt.style.available
+        stat_keys = tag_stats.values()[0]
+        for stat_key in stat_keys:
+            try:
+                values = [tag_stats[tag][stat_key]
+                    for tag in tags]
+            except KeyError as err:
+                print '?', err
+                continue
+            print values
+            plot_style = STYLES.get(stat_key, 'ro')
+            plt.plot(values, plot_style)
+            # plt.plot(tag_lengths)
         plt.title('Ansible')
         plt.xlabel('Version')
-        plt.ylabel('function length')
-        # plt.show()
+        # plt.ylabel('function length')
         plt.savefig('z.png')
         # render_func = globals()['render_{}'.format(options['style'])]
         # matchfunc = {
