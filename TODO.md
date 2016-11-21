69ec0ffada28b21f8533432819a079b719c58b59
John Mitchell <john@johntellsall.com>
2016-11-20 18:19:56 -0800

    wip

---
+++ TODO.md
@@ -0,0 +1,1 @@
+http://ggplot.yhathq.com/
--- shotglass/app/management/commands/sizeplot.py
+++ shotglass/app/management/commands/sizeplot.py
@@ -1,11 +1,17 @@
+'''
+sizeplot
+'''
+
 import re
 
 import matplotlib.pyplot as plt
 from django.core.management.base import BaseCommand
 from django.db.models import Sum
+from matplotlib.ticker import FuncFormatter, MaxNLocator
 
 from app import models
 
+# X compare with from natsort import natsorted
 
 def natural_sort_key(s, _nsre=re.compile('([0-9]+)')):
     return [int(text) if text.isdigit() else text.lower()
@@ -22,40 +28,33 @@ def get_stats(tag):
 
 class Command(BaseCommand):
     help = __doc__
+    STYLES = dict(length='r.', num_symbols='b-', num_files='g-')
 
-    # def add_arguments(self, parser):
-    #     render_styles = [name.split('_')[-1] for name in globals()
-    #         if name.startswith('render_')]
+    def add_arguments(self, parser):
+        parser.add_argument('projects', nargs='+')
 
-    #     parser.add_argument('--match', choices=('manpage', 'source'))
-    #     parser.add_argument('--num_tags', type=int)
-    #     parser.add_argument('--style', choices=render_styles, 
-    #         default='image')
-    #     parser.add_argument('project_dirs', nargs='+')
-
-    def handle(self, *args, **options):
+    def render_project(self, project):
         tags = models.SourceLine.objects.filter(
-            project__startswith='ansible-').values_list('project', flat=True).distinct()
+            project__startswith=project
+            ).values_list('project', flat=True).distinct()
         tags = sorted(tags, key=natural_sort_key)
+        tags = [tag for tag in tags if '_' not in tag]
         if 0:
             tags = tags[:10]
-        print tags
 
         tag_stats = {}
         for tag in tags:
             tag_stats[tag] = get_stats(tag)
-
-        for index, tag in enumerate(tags):
-            m = re.search(r'v(1.0|2.0.0.0-1)$', tag)
-            if not m:
-                continue
-            version = m.group(1)
-            plt.annotate(
-                'v{}'.format(version[0]), xy=(index, 20), xytext=(index, 1.5))
-                    # arrowprops=dict(facecolor='black', shrink=0.05),
-                    # )
-
-        STYLES = dict(length='r-', num_symbols='b-', num_files='g-')
+            print '{:<20} {}'.format(tag, tag_stats[tag])
+        if 0:
+            for index, tag in enumerate(tags):
+                m = re.search(r'v(1.0|2.0.0.0-1)$', tag)
+                if not m:
+                    continue
+                version = m.group(1)
+                plt.annotate(
+                    'v{}'.format(version[0]), 
+                    xy=(index, 20), xytext=(index, 1.5))
 
         plt.style.use('fivethirtyeight') # see plt.style.available
         stat_keys = tag_stats.values()[0]
@@ -66,21 +65,21 @@ class Command(BaseCommand):
             except KeyError as err:
                 print '?', err
                 continue
-            print values
-            plot_style = STYLES.get(stat_key, 'ro')
+            plot_style = self.STYLES.get(stat_key, 'ro')
             plt.plot(values, plot_style)
-            # plt.plot(tag_lengths)
-        plt.title('Ansible')
-        plt.xlabel('Version')
-        # plt.ylabel('function length')
-        plt.savefig('z.png')
-        # render_func = globals()['render_{}'.format(options['style'])]
-        # matchfunc = {
-        # # manpage -- in "man" subdir ending in .8 or .in
-        # 'manpage': Matcher(r'man/.*[0-9n]$').match,
-        # 'source': Matcher(r'\.[ch]$').match,
-        # }.get(options['match']) or all_matcher
+        plt.title(project)
+#        plt.xlabel('Version')
 
-        # for project_dir in options['project_dirs']:
-        #     repo = Repo(os.path.expanduser(project_dir))
-        #     render_func(repo, matchfunc, options)
+        # def format_fn(tick_val, tick_pos):
+        #     try:
+        #         return tags[int(tick_val)]
+        #     except IndexError:
+        #         return '?'
+        # ax = plt.figure().add_subplot(111)
+        # ax.xaxis.set_major_formatter(FuncFormatter(format_fn))
+        # ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+        plt.savefig('{}.png'.format(project))
+
+    def handle(self, *args, **options):
+        for project in options['projects']:
+            self.render_project(project)
