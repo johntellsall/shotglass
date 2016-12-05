0c2cedc7925e92e6e838d6d9174b542a17fbe917
John Mitchell <john@johntellsall.com>
2016-12-04 18:08:27 -0800

    wip

--- shotglass/app/management/commands/spider.py
+++ shotglass/app/management/commands/spider.py
@@ -34,6 +34,7 @@ def serpentine_iter(width):
             yield width - x - 1, y + 1
         y += 2
 
+
 def render_highlight(path):
     output = subprocess.check_output(
         ['source-highlight', '-i', path])
@@ -43,45 +44,31 @@ def render_highlight(path):
 
 def render(path):
     symbol_re = re.compile('<(.+?)>([^<]*)')
-    hlines = render_highlight(path)[:20]
+    hlines = render_highlight(path)
+    # hlines = hlines[:200]
     width = COL_WIDTH
     height = COL_HEIGHT
-    im = Image.new('RGB', (width, height))
-    # im_pixel = im.load()
+    im = Image.new('RGB', (width, height), color='gray')
     im_draw = ImageDraw.Draw(im)
-    colors = ['#808080']
+    colors = ['black']
     for y,line in enumerate(hlines):
         print '\t*', line
         x = 0
         mgroups = (match.groups() for match in symbol_re.finditer(line))
         for sym, text in mgroups:
             if sym.startswith('font '):
-                colors.append(sym.split('#')[1])
+                colors.append(sym.split('"')[1])
             elif sym == '/font':
                 colors.pop()
             if not text:
                 continue
+            if text.startswith(' '):
+                orig_len = len(text)
+                text = text.lstrip(' ')
+                x += orig_len - len(text)
             print colors[-1], '"{}"'.format(text)
-    # return
-
-    # width = COL_WIDTH
-    # height = COL_HEIGHT
-    # im = Image.new('RGB', (width, height))
-    # im_pixel = im.load()
-    # im_draw = ImageDraw.Draw(im)
-    # image_iter = serpentine_iter(width=width)
-    # prev_path = None
-    # color = None
-    # for func in funcs:
-    #     if func.path != prev_path:
-    #         prev_path = func.path
-    #         color = color_gen()
-    #         x,y = image_iter.next()
-    #         rect = [x-1,y-1, x+2,y+2]
-    #         im_draw.rectangle(rect, fill='black')
-    #     for _ in xrange(func.length):
-    #         im_pixel[image_iter.next()] = color
-    # return im
+            im_draw.line( (x,y, x+len(text),y), fill=colors[-1])
+    return im
 
 
 class Command(BaseCommand):
@@ -95,5 +82,5 @@ class Command(BaseCommand):
         #     CMAP_COLORS + list(reversed(CMAP_COLORS)))
 
         for path in options['paths']:
-            render(path)
-        # img.save('{}.png'.format(project))
+            img = render(path)
+            img.save('{}.png'.format('z'))
