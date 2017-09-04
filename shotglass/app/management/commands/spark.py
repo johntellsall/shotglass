8662b3f8bfd17d96cfb35e3e747276126268e355
John Mitchell <jmitchell@cargomatic.com>
2017-09-04 14:11:21 -0700

    output largest files

--- shotglass/app/management/commands/spark.py
+++ shotglass/app/management/commands/spark.py
@@ -28,14 +28,21 @@ logger = logging.getLogger(__name__)
 
 def plot(project):
     WIDTH = 20
-    query = SourceFile.objects.order_by(
-        '-num_lines').values_list('num_lines', flat=True)
-    largest, num_files = query.first(), query.count()
+    # query = SourceFile.objects.exclude(
+    #     path__contains='tests/').order_by('-num_lines')
+    query = SourceFile.objects.order_by('-num_lines')
+    num_lines = query.values_list('num_lines', flat=True)
+    largest, num_files = num_lines.first(), num_lines.count()
     print '{project}: {num_files} Python files, largest = {largest} lines'.format(
         **locals())
 
-    data = [query[i] for i in range(0, num_files, num_files/WIDTH)]
+    data = [num_lines[i] for i in range(0, num_files, num_files/WIDTH)]
     print sparklines.sparklines(data)[0]
+
+    print "Largest:"
+    for info in query[:10]:
+        print info.num_lines, info.path
+
 
 
 class Command(BaseCommand):
