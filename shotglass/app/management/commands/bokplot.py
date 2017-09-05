62d96307d44a92512576927776e90648a2da1454
John Mitchell <jmitchell@cargomatic.com>
2017-09-04 17:18:38 -0700

    new graphical plot

---
+++ shotglass/app/management/commands/bokplot.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+
+'''
+sparklines -- XX
+'''
+
+# pylint: disable=bad-builtin
+
+import logging
+import sys
+
+import django.db
+from bokeh import plotting as bplot
+from django.core.management.base import BaseCommand
+
+from app.models import SourceFile
+from app import render
+
+
+logging.basicConfig(
+    stream=sys.stderr,
+    level=logging.DEBUG,
+    format='%(asctime)s %(levelname)s %(message)s',
+    datefmt='%H:%M:%S'
+)
+logger = logging.getLogger(__name__)
+
+def plot2():
+    # prepare some data
+    x = [1, 2, 3, 4, 5]
+    y = [6, 7, 2, 4, 5]
+
+    # output to static HTML file
+    bplot.output_file("lines.html")
+
+    # create a new plot with a title and axis labels
+    p = bplot.figure(title="simple line example", x_axis_label='x', y_axis_label='y')
+
+    # add a line renderer with legend and line thickness
+    p.line(x, y, legend="Temp.", line_width=2)
+
+    # show the results
+    bplot.show(p)
+
+def plot(project):
+    WIDTH = 20
+    query = SourceFile.objects.filter(project=project).order_by('-num_lines')
+    num_lines = query.values_list('num_lines', flat=True)
+    largest, num_files = num_lines.first(), num_lines.count()
+    print '{project}: {num_files} source files, largest = {largest} lines'.format(
+        **locals())
+
+    data = [num_lines[i] for i in range(0, num_files, num_files/WIDTH)]
+    print sparklines.sparklines(data)[0]
+
+    print "Largest:"
+    for info in query[:10]:
+        print info.num_lines, info.path
+
+
+
+class Command(BaseCommand):
+    help = __doc__
+
+    def add_arguments(self, parser):
+        parser.add_argument('projects', nargs='+')
+
+    def handle(self, *args, **options):
+        plot2()
+        blam
+        projects = options['projects']
+        if projects == ['all']:
+            projects = SourceFile.projects()
+
+        for project in projects:
+            print 'PROJECT {}:'.format(project.upper())
+            plot(project)
