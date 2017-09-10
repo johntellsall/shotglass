75df672bc23c91b0409f5e76ea342ec63477eef3
John Mitchell <jmitchell@cargomatic.com>
2017-09-10 16:05:19 -0700

    add --dirindex, showing source lines per directory

--- shotglass/app/management/commands/show.py
+++ shotglass/app/management/commands/show.py
@@ -1,3 +1,6 @@
+import collections
+import os
+
 from django.core.management.base import BaseCommand
 from django.db.models import Avg, Max, Sum
 
@@ -10,9 +13,25 @@ def show_file_index(projects):
     for project in projects:
         # pylint: disable=no-member
         files = SourceFile.objects.filter(
-            project=project).order_by('name')
+            project=project).order_by('path')
         for file_ in files:
             print FORMAT.format(**vars(file_))
+
+def show_dir_index(projects):
+    """
+    for each directory, output total number of source lines
+    """
+    FORMAT = '{dir_path:50} {num_lines:>5}'
+
+    for project in projects:
+        data = SourceFile.objects.filter(
+            project=project).values_list('path', 'num_lines')
+        count = collections.Counter()
+        for path,num_lines in data:
+            count[os.path.dirname(path)] = num_lines
+
+        for dir_path,num_lines in sorted(count.iteritems()):
+            print FORMAT.format(**locals())
 
 # XX V1
 def show_symbol_index(projects):
@@ -62,6 +81,7 @@ class Command(BaseCommand):
         if 0: # XX V1
             parser.add_argument('--index', action="store_true")
         else:
+            parser.add_argument('--dirindex', action="store_true")
             parser.add_argument('--index', default=True)
 
     def handle(self, *args, **options):
@@ -74,7 +94,9 @@ class Command(BaseCommand):
         if projects == ['all']:
             projects = all_projects
 
-        if options['index']:
+        if options['dirindex']:
+            show_dir_index(projects)
+        elif options['index']:
             show_file_index(projects)
         else:
             show_summary(projects)
