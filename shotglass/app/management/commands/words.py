ee89b2a8440362a2f1f5549c9ca531aa59b847d4
John Mitchell <john@johntellsall.com>
2017-11-24 11:14:55 -0800

    wip

--- shotglass/app/management/commands/words.py
+++ shotglass/app/management/commands/words.py
@@ -7,7 +7,7 @@ import sys
 
 from django.core.management.base import BaseCommand
 
-from app.models import SourceLine
+from app.models import SourceFile
 
 
 logging.basicConfig(
@@ -19,6 +19,32 @@ logging.basicConfig(
 logging.getLogger('django.db.backends').propagate = False
 logger = logging.getLogger(__name__)
 
+def words(project):
+    camelcase_pat = re.compile('[A-Z][a-z]*')
+    print '*', project.upper()
+    namepaths = SourceFile.objects.filter(
+        project=project).values_list(
+            'name', 'path')
+    # import ipdb ; ipdb.set_trace()
+    path_words = collections.defaultdict(
+        collections.Counter)
+
+    for num,(name, path) in enumerate(namepaths):
+        names = [name.lower()]
+        if '_' in name:
+            names = name.lower().split('_')
+        elif camelcase_pat.match(name):
+            names = camelcase_pat.findall(name.lower())
+        path_words[path].update(filter(None, names))
+
+    for path, words in sorted(path_words.iteritems()):
+        relpath = re.sub('^.+?/', '', path)
+        common = [(word, count)
+                  for word, count in words.most_common(3)
+                  if count > 1]
+        if common:
+            print '{:30} {}'.format(relpath, common if common else '')
+
 
 class Command(BaseCommand):
     help = 'beer'
@@ -27,27 +53,11 @@ class Command(BaseCommand):
         parser.add_argument('projects', nargs='+')
 
     def handle(self, *args, **options):
-        camelcase_pat = re.compile('[A-Z][a-z]*')
+        projects = options['projects']
+        if projects == ['all']:
+            projects = SourceFile.projects()
         for project in options['projects']:
-            print '*', project.upper()
-            namepaths = SourceLine.objects.filter(
-                project=project).values_list(
-                    'name', 'path')
-            path_words = collections.defaultdict(
-                collections.Counter)
+            words(project)
 
-            for num,(name, path) in enumerate(namepaths):
-                names = [name.lower()]
-                if '_' in name:
-                    names = name.lower().split('_')
-                elif camelcase_pat.match(name):
-                    names = camelcase_pat.findall(name.lower())
-                path_words[path].update(filter(None, names))
 
-            for path, words in sorted(path_words.iteritems()):
-                relpath = re.sub('^.+?/', '', path)
-                common = [(word, count)
-                          for word, count in words.most_common(3)
-                          if count > 1]
-                if common:
-                    print '{:30} {}'.format(relpath, common if common else '')
+        
