3ffb8af43bb8fa276d551fd821b74a4153ae4d4a
John Mitchell <john@johntellsall.com>
2018-04-07 12:27:53 -0700

    wip: get interesting paths first

--- shotglass/app/management/commands/table.py
+++ shotglass/app/management/commands/table.py
@@ -24,29 +24,47 @@ def count_lines(blob):
     return sum(1 for line in blob.data_stream.stream.readlines())
 
 
-def do_version(tree):
+# def do_version(tree):
+#     def interestingp(i, _):
+#         return is_interesting(i.path)
+
+#     for item in tree.traverse(predicate=interestingp):
+#         yield (item.path, {'item': item, 'count': count_lines(item)})
+
+def interesting_paths(tree):
     def interestingp(i, _):
         return is_interesting(i.path)
 
     for item in tree.traverse(predicate=interestingp):
-        yield (item.path, {'item': item, 'count': count_lines(item)})
+        yield item.path
 
 
 def do_project(project):
     repo = git.Repo(project)
-    versions_names = ['0.8', '0.10', '0.12']
+    versions_labels = ['0.8', '0.10', '0.12']
 
-    versions = dict((name, repo.tags[name]) for name in versions_names)
-    detail = {}
-    for ver_label, version in versions.items():
-        detail[ver_label] = dict(do_version(version.commit.tree))
+    label = versions_labels[-1]
+    import ipdb ; ipdb.set_trace()
+    paths = list(interesting_paths(repo.tags[label].commit.tree))
 
-    latest = detail[versions_names[-1]]
-    for path in sorted(latest):
-        path_ver_count = {}
-        for ver_label, ver_detail in detail.items():
-            if path in ver_detail:
-                path_ver_count[(path, ver_label)] = ver_detail[path]['count']
+    # versions = dict((name, repo.tags[name]) for name in versions_labels)
+    # detail = {}
+    # for ver_label, version in versions.items():
+    #     detail[ver_label] = dict(do_version(version.commit.tree))
+
+    # # TODO: don't count lines of paths that aren't in latest
+    # latest = detail[versions_labels[-1]]
+    # for path in latest:
+    #     path_ver_count = {}
+    #     for ver_label, ver_detail in detail.items():
+    #         if path in ver_detail:
+    #             path_ver_count[(path, ver_label)] = ver_detail[path]['count']
+
+    for path in sorted(paths):
+        for ver_label in versions_labels:
+            version = repo.tags[ver_label]
+            import ipdb ; ipdb.set_trace()
+
         # later_item = later[path]
         # earlier_count = ''
         # if path in earlier:
