fd80eedaf91754dae042be2c4368bbfad5f7f88e
John Mitchell <john@johntellsall.com>
2018-04-07 11:16:22 -0700

    show line count in tree

--- shotglass/app/management/commands/table.py
+++ shotglass/app/management/commands/table.py
@@ -1,14 +1,13 @@
 import difflib
 import os
 import re
-import sys
 
 import git
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 
 
 def is_interesting(path):
-    if '/testsuite/' in path:
+    if re.search('(examples|scripts|testsuite)/', path):
         return False
     return os.path.splitext(path)[-1] in ['.py']
 
@@ -18,24 +17,34 @@ def get_text(blob):
         return blob.data_stream.read()
     return ''
 
+def count_lines(blob):
+    return sum(1 for line in blob.data_stream.stream.readlines())
+
+def do_version(tree):
+    def interestingp(i, _):
+        return is_interesting(i.path)
+
+    for item in tree.traverse(predicate=interestingp):
+        num_lines = count_lines(item)
+        print(f'{num_lines}\t{item.path}')
+
 def do_project(project):
     repo = git.Repo(project)
-    diffs = repo.commit('0.8').diff('0.9')
-    diffs = [d for d in diffs if is_interesting(d.b_path)]
-    for diff in diffs:
-        print('{}'.format(diff.b_path))
-        a_text = get_text(diff.a_blob)
-        b_text = get_text(diff.b_blob)
-        try:
-            parts = difflib.Differ().compare(a_text, b_text)
-            print(", ".join(parts))
-        except Exception as exc:
-            print('?', exc)
+    do_version(repo.tags['0.9'].commit.tree)
+    # diffs = repo.commit('0.8').diff('0.9')
+    # diffs = [d for d in diffs if is_interesting(d.b_path)]
+    # for diff in diffs:
+    #     print('{}'.format(diff.b_path))
+    #     a_text = get_text(diff.a_blob)
+    #     b_text = get_text(diff.b_blob)
+    #     try:
+    #         parts = difflib.Differ().compare(a_text, b_text)
+    #         print(", ".join(parts))
+    #     except Exception as exc:
+    #         print('?', exc)
         # import ipdb ; ipdb.set_trace()
         # print('{}\t{}'.format(diff.b_path, 
         #     diff.b_blob.data_stream.read()[:40]))
-    import ipdb ; ipdb.set_trace()
-    pass
 
 
 class Command(BaseCommand):
