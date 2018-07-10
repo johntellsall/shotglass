643274388f99d36f7b63c3ea50e22effcef8c6ec
John Mitchell <john@johntellsall.com>
2018-07-09 21:03:24 -0700

    add column to dataframe

--- ex-treemap/tree.py
+++ ex-treemap/tree.py
@@ -22,6 +22,7 @@ def count_lines(path):
     return sum(1 for line in open(path))
 
 
+# TODO make configurable
 def walk_tree(topdir):
     for root, dirs, files in os.walk(topdir):
         dirs[:] = list(set(dirs) - DULL_DIRECTORIES)
@@ -29,21 +30,22 @@ def walk_tree(topdir):
             yield os.path.join(root, file)
 
 
+# TODO make configurable
 def is_source(path):
     return os.path.splitext(path)[-1] == '.py'
 
 
-
 source_paths = list(filter(is_source, walk_tree(sys.argv[1])))
-print(len(source_paths)); sys.exit(1)
 line_counts = list(map(count_lines, source_paths))
 names = list(map(os.path.basename, source_paths))
-print(line_counts)
 print(names)
 df = pd.DataFrame({
-    'lines': line_counts,
-    'name': names,
-})
-squarify.plot(sizes=df['lines'], label=df['name'], alpha=.8)
+    'paths': source_paths,
+    'names': names})
+df['line_counts'] = line_counts
+
+squarify.plot(
+    sizes=df['line_counts'],
+    label=df['names'], alpha=.8)
 plt.axis('off')
-plt.savefig('dir.png')
+plt.savefig('tree.png')
