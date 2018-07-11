f44d063d851238181272152f1e0345adb2493955
John Mitchell <john@johntellsall.com>
2018-07-10 22:10:44 -0700

    compile to dataframe, write as pickle

--- ex-treemap/make_index.py
+++ ex-treemap/make_index.py
@@ -1,6 +1,7 @@
 import logging
 import multiprocessing.pool as mpool
 import os
+import pandas as pd
 import subprocess
 import sys
 import time
@@ -34,31 +35,62 @@ def make_ctags(path):
     cmd = format_index_cmd(path)
     out = subprocess.check_output(cmd)
     out = out.decode("utf-8", errors='ignore')
-    return {"path": path, "ctags": out}
+    return out
+    # return {"path": path, "ctags": out}
+
 
 def make_linecount(path):
     count = count_lines(path)
-    return {"path": path, "lines": count}
+    return count
+    # return {"path": path, "lines": count}
 
 # TODO make configurable
+
+
 def is_source(path):
     return os.path.splitext(path)[-1] in GOOD_EXTENSIONS
 
+def compile(project_dir):
+    source_paths = list(filter(is_source, walk_tree(project_dir)))
+    logging.info((f'{project_dir}: {len(source_paths)} files'))
+    pool = mpool.Pool()
+    start_tm = time.time()
+    ctags_rows = pool.map(make_ctags, source_paths)
+    count_rows = pool.map(make_linecount, source_paths)
+    pool.close()
+    pool.join()
+    elapsed_tm = time.time() - start_tm
+    logging.info('done')
+    logging.info('%.1f files / sec', len(source_paths)/elapsed_tm)
 
-logging.basicConfig(datefmt='%X', format=LOG_FORMAT, level=logging.INFO)
+    df = pd.DataFrame({
+        'path': source_paths,
+        'ctags': ctags_rows,
+        'linecount': count_rows,
+    })
+    return df
 
-project_dir = sys.argv[1]
-source_paths = list(filter(is_source, walk_tree(project_dir)))
-logging.info((f'{project_dir}: {len(source_paths)} files'))
-pool = mpool.Pool()
-start_tm = time.time()
-result1 = pool.map(make_ctags, source_paths)
-result2 = pool.map(make_linecount, source_paths)
-pool.close()
-pool.join()
-elapsed_tm = time.time() - start_tm
-logging.info('done')
-logging.info('%.1f files / sec', len(source_paths)/elapsed_tm)
-if 1:
-    print(result1[0])
-    print(result2[0])
+def main(project_dir):
+    logging.basicConfig(datefmt='%X', format=LOG_FORMAT, level=logging.INFO)
+
+    project_dir = sys.argv[1]
+    project_name = os.path.basename(project_dir)
+    project_data_path = f'{project_name}.pkl'
+
+    if os.path.exists(project_data_path):
+        df = pd.read_pickle(project_data_path)
+    else:
+        df = compile(project_dir)
+        df.to_pickle(project_data_path)
+    import ipdb ; ipdb.set_trace()
+
+
+if __name__=='__main__':
+    main(sys.argv[1])
+
+
+# df = pd.DataFrame({'path': [8, 3, 4, 2],
+#     'group': [
+# if 1:
+#     print(result1[0])
+#     print(result2[0])
