22de65a9ff78497a564b6336550740368f36ac56
John Mitchell <john@johntellsall.com>
2018-07-10 21:43:41 -0700

    count lines as well as ctags

--- ex-treemap/make_index.py
+++ ex-treemap/make_index.py
@@ -1,10 +1,15 @@
 import logging
-import os
-import sys
 import multiprocessing.pool as mpool
+import os
+import subprocess
+import sys
+import time
+
 
 DULL_DIRECTORIES = set(['.git'])
+GOOD_EXTENSIONS = ('.py', '.c', '.go')
 LOG_FORMAT = '%(asctime)-15s %(message)s'
+
 
 def count_lines(path):
     return sum(1 for line in open(path))
@@ -17,20 +22,43 @@ def walk_tree(topdir):
         for file in files:
             yield os.path.join(root, file)
 
-def make_index(path):
-    return path
+# ctags -o - --extras=* --fields='*' --pseudo-tags='TAG_KIND_DESCRIPTION'
+
+
+def format_index_cmd(path):
+    cmd = 'ctags -o -'.split()
+    return cmd + [path]
+
+
+def make_ctags(path):
+    cmd = format_index_cmd(path)
+    out = subprocess.check_output(cmd)
+    out = out.decode("utf-8", errors='ignore')
+    return {"path": path, "ctags": out}
+
+def make_linecount(path):
+    count = count_lines(path)
+    return {"path": path, "lines": count}
 
 # TODO make configurable
 def is_source(path):
-    return os.path.splitext(path)[-1] in ('.py', '.c')
+    return os.path.splitext(path)[-1] in GOOD_EXTENSIONS
+
 
 logging.basicConfig(datefmt='%X', format=LOG_FORMAT, level=logging.INFO)
 
 project_dir = sys.argv[1]
 source_paths = list(filter(is_source, walk_tree(project_dir)))
-logging.info((f'{project_dir}: {len(source_paths)} paths'))
+logging.info((f'{project_dir}: {len(source_paths)} files'))
 pool = mpool.Pool()
-print(pool.map(make_index, source_paths))
+start_tm = time.time()
+result1 = pool.map(make_ctags, source_paths)
+result2 = pool.map(make_linecount, source_paths)
 pool.close()
 pool.join()
+elapsed_tm = time.time() - start_tm
 logging.info('done')
+logging.info('%.1f files / sec', len(source_paths)/elapsed_tm)
+if 1:
+    print(result1[0])
+    print(result2[0])
