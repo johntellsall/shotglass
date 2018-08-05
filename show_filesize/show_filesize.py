64cd4d114ae7ff30d7440a9c32785a5ca0c224b8
John Mitchell <john@johntellsall.com>
2018-08-04 18:20:57 -0700

    output number of bytes

--- show_filesize/show_filesize.py
+++ show_filesize/show_filesize.py
@@ -7,10 +7,14 @@ show_filesize.py -- show source sizes over time
 import os
 import re
 import sys
+from collections import namedtuple
 
 from colorama import Back, Fore, Style
 from git import Repo
 from natsort import natsorted
+
+
+Source = namedtuple('Source', 'path lines')
 
 
 def count_lines(path):
@@ -30,12 +34,8 @@ def is_interesting_path(path):
         return False
     return True
 
-# def calc_stripe(tree):
 
-repo = Repo(sys.argv[1])
-for tag in natsorted(repo.tags, key=lambda t: t.name):
-    print(Back.GREEN + Fore.BLACK+ f'### {tag.name}' + Style.RESET_ALL)
-
+def calc_stripe(tree):
     for item in tag.commit.tree.traverse():
         if item.type != 'blob':
             continue
@@ -43,6 +43,15 @@ for tag in natsorted(repo.tags, key=lambda t: t.name):
             continue
         if not is_interesting_name(item.name):
             continue
-        # import ipdb ; ipdb.set_trace()
-        num_bytes = item.size
-        print(f'{num_bytes:5} {item.path}')
+        import ipdb ; ipdb.set_trace()
+        num_lines = count_lines(item.path)
+        yield Source(path=item.path, lines=num_lines)
+
+
+repo = Repo(sys.argv[1])
+column = {}
+for tag in natsorted(repo.tags, key=lambda t: t.name):
+    print(Back.GREEN + Fore.BLACK + f'### {tag.name}' + Style.RESET_ALL)
+    stripe = calc_stripe(tag.commit.tree)
+    for source in stripe:
+        print(f'{source.lines:5} {source.path}')
