d8e055befdeaab1d2fcfb92bfa15cd9d659be891
John Mitchell <john@johntellsall.com>
2018-08-04 17:55:11 -0700

    list interesting files

--- show_filesize/show_filesize.py
+++ show_filesize/show_filesize.py
@@ -1,12 +1,41 @@
 #!/usr/bin/env python
 
+'''
+show_filesize.py -- show source sizes over time
+'''
+
+import os
+import re
 import sys
+
+from colorama import Fore, Style
 from git import Repo
 from natsort import natsorted
 
+
+# TODO make configurable
+def is_interesting_name(name):
+    return os.path.splitext(name)[-1] in ['.py']
+
+
+# TODO make configurable
+def is_interesting_path(path):
+    if re.compile(r'^(docs|examples|scripts|tests)/').match(path):
+        return False
+    if re.compile('/testsuite/').search(path):
+        return False
+    return True
+
+
 repo = Repo(sys.argv[1])
-# tags = [t.name for t in repo.tags]
 for tag in natsorted(repo.tags, key=lambda t: t.name):
-    print(tag.name, tag.commit.stats.total)
-# tags = ['0.4', '0.8', '0.12.2']
-# import ipdb ; ipdb.set_trace()
+    print(Fore.GREEN + f'### {tag.name}' + Style.RESET_ALL)
+
+    for item in tag.commit.tree.traverse():
+        if item.type != 'blob':
+            continue
+        if not is_interesting_path(item.path):
+            continue
+        if not is_interesting_name(item.name):
+            continue
+        print(item.path)
