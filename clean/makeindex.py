249538fb19530b75787ee1b3592f44ab45e21793
John Mitchell <john@johntellsall.com>
2018-11-17 18:35:03 -0800

    switch to gitpython module

--- clean/makeindex.py
+++ clean/makeindex.py
@@ -1,22 +1,13 @@
-import subprocess
-from pathlib import Path
+import git
+
+# from git import Repo
 
 # TODO: raise exception if command failed
-def system(cmd):
-    print(">>>", cmd)
-    out_text = subprocess.getoutput(cmd)
-    return out_text.split(r"\n")
+# def system(cmd):
+#     print(">>>", cmd)
+#     out_text = subprocess.getoutput(cmd)
+#     return out_text.split(r"\n")
 
-
-class Repos:
-    def __init__(self, git_dir):
-        maybe_dir = Path(git_dir) / ".git"
-        if maybe_dir.is_dir():
-            git_dir = maybe_dir
-        self.git_dir = git_dir
-
-    def cmd(self, args):
-        return ["git", f"--git-dir={self.git_dir}"] + list(args)
-
-    def ls_files(self):
-        return system(self.cmd(["ls-files"]))
+git_dir = ".."
+g = git.Git(git_dir)
+rval = g.ls_files()
--- clean/test_makeindex.py
+++ clean/test_makeindex.py
@@ -1,7 +1,7 @@
 import makeindex
 
 
-def test_ls_files():
-    out = makeindex.Repos("..").ls_files()
-    print(out)
-    assert type(out) is list and len(out) > 0
+# def test_ls_files():
+#     out = makeindex.Repos("..").ls_files()
+#     print(out)
+#     assert type(out) is list and len(out) > 0
