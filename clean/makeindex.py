7b03bfbaba6af29408765346ec4eeb1b03e058fa
John Mitchell <john@johntellsall.com>
2018-11-17 19:14:41 -0800

    ctags and git

--- clean/makeindex.py
+++ clean/makeindex.py
@@ -1,3 +1,5 @@
+import subprocess
+
 import git
 
 # from git import Repo
@@ -9,7 +11,20 @@ import git
 #     return out_text.split(r"\n")
 
 
-def main(project_dir):
+def list_source_files(project_dir):
     g = git.Git(project_dir)
     rval = g.ls_files("*.py", "*.c").split(r"\n")
     return rval
+
+
+def make_index(source_paths):
+    cmd = "ctags --languages=python,c,go --excmd=number".split()
+    cmd += source_paths
+    out = subprocess.run(cmd, check=True)
+    print(out)
+
+
+# find /Users/johnmitchell/src/SOURCE/flask
+# | ctags --languages=python,c,go --filter --excmd=number
+
+# tag_name<TAB>file_name<TAB>ex_cmd;"<TAB>extension_fields
--- clean/test_makeindex.py
+++ clean/test_makeindex.py
@@ -6,12 +6,16 @@ SOURCE_DIR = Path("/Users/johnmitchell/src/SOURCE")
 
 
 def test_ls_files_py():
-    out = makeindex.main(Path(".."))
+    out = makeindex.list_source_files(Path(".."))
     print(out)
     assert type(out) is list and len(out) > 0
 
 
 def test_ls_files_c():
-    out = makeindex.main(SOURCE_DIR / "redis")
+    out = makeindex.list_source_files(SOURCE_DIR / "redis")
     print(out)
     assert type(out) is list and len(out) > 0
+
+
+def test_make_index():
+    makeindex.make_index(["makeindex.py"])
