16c110ecd9d1d7cd1c09065c344b3779aca5631b
John Mitchell <john@johntellsall.com>
2019-06-01 21:48:30 -0700

    count files in first, last releases

--- show_filesize/show_info.py
+++ show_filesize/show_info.py
@@ -14,16 +14,9 @@ from git import Repo
 from natsort import natsorted
 
 
-Source = namedtuple('Source', 'path lines')
-
-
-def count_lines(lines):
-    return sum(1 for line in lines)
-
-
 # TODO make configurable
 def is_interesting_name(name):
-    return os.path.splitext(name)[-1] in ['.py']
+    return os.path.splitext(name)[-1] in ['.c', '.py']
 
 
 # TODO make configurable
@@ -34,36 +27,31 @@ def is_interesting_path(path):
         return False
     return True
 
+def is_interesting_item(item):
+    return (item.type == 'blob' and
+        is_interesting_path(item.path) and
+        is_interesting_name(item.name))
 
 def find_sources(tree):
     # "calc info about every file in tree"
-    for item in tree.traverse():
-        if item.type != 'blob':
-            continue
-        if not is_interesting_path(item.path):
-            continue
-        if not is_interesting_name(item.name):
-            continue
-        stream = item.data_stream.stream
-        num_lines = count_lines(stream.readlines())
-        yield Source(path=item.path, lines=num_lines)
+    source_items = tree.traverse(predicate=lambda item,_: is_interesting_item(item))
+    return source_items
 
 def by_name(source):
     return source.name
 
-def adjust_columns(cols, sources):
-    "add new files to the end of the row"
-    for source in sources:
-        if source.path not in cols:
-            print(f'+ {source.path}')
-            cols[source.path] = len(cols)
+def count_release(release):
+    return len(list(find_sources(release.commit.tree)))
 
-# TODO only count the N-largest files in the last tag tree
+
 repo = Repo(sys.argv[1])
 releases = natsorted(repo.tags, key=by_name)
 print(f'releases: {len(releases)}')
-print(f'first: {releases[0]}')
-print(f'last: {releases[-1]}')
+
+num_first = count_release(releases[0])
+num_last = count_release(releases[-1])
+print(f'first: {releases[0]} num_files: {num_first}')
+print(f'last: {releases[-1]} num_files: {num_last}')
 sys.exit(1)
 path_column = {}
 for tag in natsorted(repo.tags, key=by_name):
