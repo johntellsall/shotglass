3418a59da007cf58fecf38df8e70e3800ac69fc3
John Mitchell <john@johntellsall.com>
2019-06-10 19:54:21 -0700

    docs

--- ex-treemap/make_squarify.py
+++ ex-treemap/make_squarify.py
@@ -1,25 +1,30 @@
 #!/usr/bin/env python
 
-# make_squarify -- TBD
+# make_squarify -- render image with squares representing source files
+#
+# USAGE:
+# make_squarify flask.pkl
+# -- outputs flask.png
 
 import os
 import sys
 from collections import Counter
 
 import matplotlib
-matplotlib.use('agg')
+
+matplotlib.use("agg")
 
 import matplotlib.pyplot as plt
 import pandas as pd
 import squarify
 
-PREFIX = '/Users/johnmitchell/jsrc/shotglass/SOURCE/'
+PREFIX = "/Users/johnmitchell/jsrc/shotglass/SOURCE/"
 
 
 def zap_prefix(path):
     if not path.startswith(PREFIX):
         return path
-    rest = path[len(PREFIX):]
+    rest = path[len(PREFIX) :]
     # TODO strip next dir name
     return rest
 
@@ -27,10 +32,10 @@ def zap_prefix(path):
 def make_squarify_files(project_data_path):
     df = pd.read_pickle(project_data_path)
     df = df[df.linecount > 0]
-    squarify.plot(sizes=df.linecount, label=df.name, alpha=.8)
-    plt.axis('off')
+    squarify.plot(sizes=df.linecount, label=df.name, alpha=0.8)
+    plt.axis("off")
     name = os.path.splitext(os.path.basename(project_data_path))[0]
-    out_path = f'{name}.png'
+    out_path = f"{name}.png"
     print(out_path)
     plt.savefig(out_path)
 
@@ -44,31 +49,18 @@ def make_squarify(project_data_path):
         dir_size[dir_] += item.linecount
 
     items = dir_size.most_common()  # sort
-    dir_items = pd.DataFrame(items, columns=('dir', 'dir_linecount'))
+    dir_items = pd.DataFrame(items, columns=("dir", "dir_linecount"))
 
     dir_items = dir_items[dir_items.dir_linecount > 0]
-    squarify.plot(
-        sizes=dir_items.dir_linecount,
-        label=dir_items.dir,
-        alpha=.8)
-    plt.axis('off')
-    
+    squarify.plot(sizes=dir_items.dir_linecount, label=dir_items.dir, alpha=0.8)
+    plt.axis("off")
+
     name = os.path.splitext(os.path.basename(project_data_path))[0]
     plt.title(name.title())
-    out_path = f'{name}.png'
+    out_path = f"{name}.png"
     print(out_path)
     plt.savefig(out_path)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     make_squarify(sys.argv[1])
-
-# source_paths = list(filter(is_source, sys.argv[1:]))
-# line_counts = list(map(count_lines, source_paths))
-# names = list(map(os.path.basename, source_paths))
-# print(line_counts)
-# print(names)
-# df = pd.DataFrame({
-#     'lines': line_counts,
-#     'name': names,
-# })
