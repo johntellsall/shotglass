7ed7004cfed9d4809d1014df24d5e6da9dd78084
John Mitchell <john@johntellsall.com>
2020-05-12 21:41:59 -0700

    scan multiple projects

---
+++ 2020/test_summarize.py
@@ -0,0 +1,33 @@
+import subprocess
+from pathlib import Path
+
+def source(name):
+    # return f'../SOURCE/{name}'
+    return f'../SOURCE/{name}'
+
+def system(cmd):
+    return subprocess.run(cmd, capture_output=True, check=True,
+    universal_newlines = True)
+
+def cmd_git_list_date_tags(project_dir):
+    return ['git', f'--git-dir={Path(project_dir) / ".git"}',
+    'log', '--date-order', '--graph',
+    '--tags', '--simplify-by-decoration', 
+	'--pretty=format:"%ai %d"']
+def main():
+    projects = [source(n) for n in ['openssh-portable', 'dhcp']]
+    for project in projects:
+        print(f'{project} :::::')
+        x = system(cmd_git_list_date_tags(project))
+        lines = x.stdout.splitlines()
+        print(f'{len(lines)} lines')
+        print('\n'.join(lines[:3]))
+        print('...')
+        print('\n'.join(lines[-3:]))
+
+def test_main():
+    main()
+
+if __name__ == '__main__':
+    main()
+
