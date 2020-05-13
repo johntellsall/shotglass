68922ef35adbce0eb0b45391bf27700fbb25de36
John Mitchell <john@johntellsall.com>
2020-05-12 22:20:11 -0700

    count tag lines

--- 2020/test_summarize.py
+++ 2020/test_summarize.py
@@ -1,4 +1,6 @@
+import glob
 import subprocess
+import sys
 from pathlib import Path
 from colorama import Fore, Back, Style
 
@@ -13,22 +15,41 @@ def system(cmd):
     universal_newlines = True)
 
 def cmd_git_list_date_tags(project_dir):
-    return ['git', f'--git-dir={Path(project_dir) / ".git"}',
+    git_dir = Path(project_dir) / ".git"
+    if not git_dir.exists():
+        raise ValueError("Git dir missing")
+    return ['git', f'--git-dir={git_dir}',
     'log', '--date-order', '--graph',
     '--tags', '--simplify-by-decoration', 
 	'--pretty=format:"%ai %d"']
 
 def main():
-    projects = [source(n) for n in ['openssh-portable', 'dhcp']]
+    if 0:
+        projects = [source(n) for n in ['openssh-portable', 'dhcp']]
+    else:
+        projects = glob.glob(source('[a-z]*'))
+
+    print(f'{Fore.YELLOW}{len(projects)} PROJECTS :::::')
+    print(Style.RESET_ALL)
+
     for project in projects:
-        print(f'{Fore.YELLOW}{project} :::::')
-        print(Style.RESET_ALL)
-        x = system(cmd_git_list_date_tags(project))
+        print(f'{Fore.YELLOW}{project} :::::{Style.RESET_ALL}')
+        try:
+            x = system(cmd_git_list_date_tags(project))
+        except ValueError:
+            print(f'{Fore.RED}missing{Style.RESET_ALL}')
+            continue
+        except subprocess.CalledProcessError:
+            print(f'{Fore.RED}{x.returncode}{Style.RESET_ALL}')
+            continue
+          
         lines = x.stdout.splitlines()
-        print(f'{len(lines)} lines')
-        print('\n'.join(lines[:3]))
-        print('...')
-        print('\n'.join(lines[-3:]))
+        lines = [line for line in lines if 'tag:' in line]
+        print(f'{len(lines)} tag lines')
+        if 1:
+            print('\n'.join(lines[:3]))
+            print('...')
+            print('\n'.join(lines[-3:]))
 
 def test_main():
     main()
