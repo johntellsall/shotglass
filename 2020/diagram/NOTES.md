ed9000e3f1e0e71b9c9f2eb4ffa2389a7bd6fb38
John Mitchell <john@johntellsall.com>
2020-07-24 22:04:48 -0700

    count file types

---
+++ 2020/diagram/NOTES.md
@@ -0,0 +1,19 @@
+#
+
+# Distro
+
+## download full package list
+
+https://distrowatch.com/table.php?distribution=mint&pkglist=true&version=20#pkglist
+
+
+# Notes
+
+* coreutils = true, date, hostname, sleep, mkdir
+
+## Resource: Black Duck
+
+* https://www.openhub.net/p/redis
+
+* has an API! Ohloh
+  * https://github.com/blackducksoftware/ohloh_api#ohloh-api-documentation
--- 2020/diagram/index.py
+++ 2020/diagram/index.py
@@ -6,9 +6,15 @@ from git import Git, Repo
 
 SOURCE = pathlib.Path('~/jsrc/shotglass/SOURCE').expanduser()
 
+def ls_files(gitobj, pat):
+    return gitobj.ls_files(pat).split('\n')
 
 for project in SOURCE.glob('flask'):
     print(project.name)
-    g = Git( project )
-    rval = g.ls_files('*.py')
-    import ipdb; ipdb.set_trace()
+    gitproj = Git(project)
+    py_count = len(ls_files(gitproj, '*.py'))
+    c_count = len(ls_files(gitproj, '*.c'))
+    total_count = len(ls_files(gitproj, '*'))
+
+    print(f'{project.name} {total_count} {py_count} {c_count}')
+    # import ipdb; ipdb.set_trace()
