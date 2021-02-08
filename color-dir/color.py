8ec04c71ea03b556d383438d4029eb9094a2f90d
John Mitchell <john@johntellsall.com>
2021-02-07 20:37:45 -0800

    demo

--- color-dir/color.py
+++ color-dir/color.py
@@ -1,10 +1,18 @@
 from PIL import Image, ImageDraw
 
-# with Image.open("z.jpg") as im:
-img = Image.new("RGB", (100, 100), (250, 250, 250))
+img = Image.new("RGB", (100, 100), (88, 88, 88))
 
 draw = ImageDraw.Draw(img)
-draw.line((0, 0) + img.size, fill=128)
-draw.line((0, img.size[1], img.size[0], 0), fill=128)
+if 0:
+    draw.line((0, 0) + img.size, fill="red")
+    draw.line((0, img.size[1], img.size[0], 0), fill="blue")
+
+if 0:
+    draw.point((75, 50), fill="red")
+
+draw.rectangle([20, 50, 30, 65], fill="green", outline="red", width=2)
+
+if 0:
+    draw.text([20, 50], "Beer", fill="black")
 
 img.save("z.png", "PNG")
