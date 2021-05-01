6d4be5e9c0e3f57e142a26d05fe8daf6691f34b0
John Mitchell <john@johntellsall.com>
2021-04-30 23:14:44 -0700

    parse

--- may/may.py
+++ may/may.py
@@ -38,21 +38,26 @@ def run_ctags(path):
     return proc.stdout
 
 
+def parse_ctags(blob):
+    # "tests_require	..//setup.py	...	kind:variable"
+    var_pat = re.compile(r"^(?P<name> \S+) .* kind:(?P<kind> \S+)", re.VERBOSE)
+    return var_pat.finditer(blob)
+
+
 def main():
     project_dir = Path(sys.argv[1])
     repos = git.Repo(project_dir)
     tree = repos.heads.master.commit.tree
     print(project_dir)
-    for entry in tree:
-        if not entry.path.endswith(".py"):
-            continue
+    source = [item for item in tree if item.path.endswith(".py")]
+    for entry in source:
         print(entry)
         print(f"{entry.path} {entry.size}")
         fullpath = project_dir / entry.path
-        ctags = run_ctags(fullpath)
-        print(ctags)
-        blam
-        # breakpoint()
+        ctags_text = run_ctags(fullpath)
+        for tag in parse_ctags(ctags_text):
+            print(tag.groupdict())
+    print("DONE")
 
 
 if __name__ == "__main__":
