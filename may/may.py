71669dcad4c5e44ef2f402549ac5f353df5c0793
John Mitchell <john@johntellsall.com>
2021-04-30 21:50:15 -0700

    print project top directory

---
+++ may/may.py
@@ -0,0 +1,37 @@
+import sqlite3
+import sys
+from pathlib import Path
+
+
+def setup(db):
+    db.execute(
+        """
+    CREATE TABLE files (path text, line_count int)
+    """
+    )
+
+
+def show_info(db):
+    for row in db.execute("SELECT * FROM files ORDER BY 1"):
+        print(row)
+
+
+def db_demo():
+    con = sqlite3.connect("may.db")
+    cur = con.cursor()
+
+    cur.execute("insert into files values (?, ?)", ("C", 49))
+
+    con.commit()
+
+    show_info(con)
+    con.close()
+
+
+def main():
+    project_dir = Path(sys.argv[1])
+    print(project_dir)
+
+
+if __name__ == "__main__":
+    main()
