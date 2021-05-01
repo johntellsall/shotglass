b948938c8b752f7c92d0e299891e086e4dfcf493
John Mitchell <john@johntellsall.com>
2021-05-01 15:55:08 -0700

    wip: parse all symbols!

--- may/may.py
+++ may/may.py
@@ -40,11 +40,10 @@ def run_ctags(path):
 
 def parse_ctags(blob):
     # "tests_require	..//setup.py	...	kind:variable"
-    tag_exp = r"^(?P<name> \S+) .*? kind:(?P<kind> \S+)"
+    tag_exp = r"(?P<name> \S+) .*? kind:(?P<kind> \S+)"
     tag_pat = re.compile(tag_exp, re.VERBOSE)
     tag_iter = tag_pat.finditer(blob)
-    breakpoint()
-    tag_iter = tag_iter
+    return tag_iter
 
 
 def list_paths(repo):
@@ -72,14 +71,14 @@ def format_summary(tags):
     return f"{len(tags)} tags"
 
 
-def main_scan_project():
+def main():
     project_dir = Path(sys.argv[1])
     print(project_dir)
     repo = git.Repo(project_dir)
     tree = repo.heads.master.commit.tree
     source_paths = filter(is_source_path, list_paths(repo))
     source_paths = filter(is_interesting, source_paths)
-    for path in ["pyramid/view.py"]:  # list(source_paths)[:100]:
+    for path in list(source_paths)[:100]:
         entry = tree[path]
         print(f"{entry.path} {entry.size}")
         fullpath = project_dir / entry.path
@@ -94,25 +93,25 @@ def main_scan_project():
     print("DONE")
 
 
-def main():
-    project_dir = sys.argv[1]
-    repo = git.Repo(project_dir)
-    tree = repo.heads.master.commit.tree
+# def main():
+#     project_dir = sys.argv[1]
+#     repo = git.Repo(project_dir)
+#     tree = repo.heads.master.commit.tree
 
-    source_path = sys.argv[2]
-    for path in [source_path]:
-        entry = tree[path]
-        print(f"{entry.path} {entry.size}")
-        fullpath = Path(project_dir) / entry.path
-        ctags_text = run_ctags(fullpath)
-        tags = list(parse_ctags(ctags_text))
-        if not tags:
-            print("?")
-        else:
-            print([tag["name"] for tag in tags])
-            print("woot")
-        # print(format_summary(tags))
-    print("DONE")
+#     source_path = sys.argv[2]
+#     for path in [source_path]:
+#         entry = tree[path]
+#         print(f"{entry.path} {entry.size}")
+#         fullpath = Path(project_dir) / entry.path
+#         ctags_text = run_ctags(fullpath)
+#         tags = list(parse_ctags(ctags_text))
+#         if not tags:
+#             print("?")
+#         else:
+#             print([tag["name"] for tag in tags])
+#             print("woot")
+#         # print(format_summary(tags))
+#     print("DONE")
 
 
 if __name__ == "__main__":
