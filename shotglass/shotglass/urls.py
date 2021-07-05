e1ba1c8f6c17f72c936f06d8f1b1d06fb84b411a
John Mitchell <john@johntellsall.com>
2021-07-05 13:38:20 -0700

    add beer command for tasting I mean testing

--- shotglass/shotglass/urls.py
+++ shotglass/shotglass/urls.py
@@ -1,9 +1,7 @@
 # shotglass.urls
 
 from django.conf.urls import include, re_path
-# from django.contrib import admin
 
 urlpatterns = [
     re_path(r"^app/", include("app.urls")),
-    #    url(r'^admin/', include(admin.site.urls)),
 ]
