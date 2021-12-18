7ecf34326c42c3106e93b39d167cf7a4b3a7e549
John Mitchell <john@johntellsall.com>
2021-12-18 13:24:34 -0800

    more plot test

--- dec/s-grouped_boxplot.py
+++ dec/s-grouped_boxplot.py
@@ -4,12 +4,19 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 sns.set_theme(style="ticks", palette="pastel")
+plt.figure(figsize=(15, 8))
 
 # Load the example tips dataset
 tips = sns.load_dataset("tips")
 
 # Draw a nested boxplot to show bills by day and time
-sns.boxplot(x="day", y="total_bill", hue="smoker", palette=["m", "g"], data=tips)
+sns.boxplot(
+    x="day",
+    y="total_bill",
+    hue="smoker",
+    palette=["m", "g"],
+    data=tips,
+)
 # sns.despine(offset=10, trim=True)
 
 import sys
