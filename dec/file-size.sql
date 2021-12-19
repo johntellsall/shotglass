d0ba9abdf27305d064866b005610be7cee20ddac
John Mitchell <john@johntellsall.com>
2021-12-18 22:01:35 -0800

    wip

--- dec/file-size.sql
+++ dec/file-size.sql
@@ -1,9 +1,19 @@
-.schema files
+-- .schema files
 
-select '';
 select 'Project File Sizes';
 
-select projects.name, path, byte_count
+select projects.name as project_name, path, byte_count
 from files, projects
 on files.project_id = projects.id
-where project_id in (1,2) limit 3;
+where project_id in (1,2)
+limit 10;
+
+select '';
+select 'Projects';
+select '-- C';
+
+select name,size_mb from proj_summary where language='C' and size_mb < 200 order by 2;
+
+select '';
+select '-- Python';
+select name,size_mb from proj_summary where language='Python' order by 2;
