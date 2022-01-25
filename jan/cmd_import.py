eb937fa1ae58376d4db36fa24698acb32a8d74dd
John Mitchell <john@johntellsall.com>
2022-01-24 20:56:51 -0800

    import

--- jan/cmd_import.py
+++ jan/cmd_import.py
@@ -1,15 +1,41 @@
 import subprocess
 import sys
+import sqlite3
 
 
-def cmd_import(path):
-    cmd = "git ls-tree -r --long '2.0.0'"
-    cmd = f"git -C {path} ls-tree  -r --long '2.0.0'"
+def get_git_release_blob(path, release="2.0.0"):
+    cmd = f"git -C {path} ls-tree  -r --long '{release}'"
     proc = subprocess.run(cmd, shell=True, capture_output=True, text=True, check=True)
     # if verbose:
     #     print(f"-- RAW\n{proc.stdout[:300]}\n-- ENDRAW")
-    assert 0, proc.stdout
+    return proc.stdout
+
+
+def setup(db):
+    db.execute("drop table if exists file_hash")  # <== XXXXX
+
+    db.execute("create table file_hash(path, hash, size_bytes)")
+
+
+def main(path):
+    con = sqlite3.connect("jan.db")
+    setup(con)
+
+    blob = get_git_release_blob(path)
+
+    def row2item(row):
+        pre, path = row.split("\t")
+        _mode, _type, hash, size_bytes = pre.split()
+        return path, hash, size_bytes
+
+    data = list(map(row2item, blob.rstrip("\n").split("\n")))
+
+    con.executemany(
+        "insert into file_hash(path, hash, size_bytes) values (?, ?, ?)", data
+    )
+
+    print(list(con.execute("select count(*) from file_hash")))
 
 
 if __name__ == "__main__":
-    cmd_import(sys.argv[1])
+    main(sys.argv[1])
