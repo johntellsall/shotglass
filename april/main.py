97c74a8d572d3513e734e8e7f9701930f58eac84
John Mitchell <john@johntellsall.com>
2022-04-14 21:30:31 -0700

    wip

--- april/main.py
+++ april/main.py
@@ -4,42 +4,16 @@
 function(path) -> data
 """
 
-import json
 import logging
 import pprint
-import subprocess
 import re
 from pathlib import PurePath
 import click
 from distutils.version import LooseVersion
 
-# Universal Ctags
-CTAGS_ARGS = "ctags --output-format=json --fields=*-P -o -".split()
+import run
 
 logging.basicConfig(format="%(asctime)-15s %(message)s", level=logging.INFO)
-
-
-def run_ctags(path, verbose=False):
-    "Ctags command output -- iter of dictionaries, one per symbol"
-    cmd = CTAGS_ARGS + [path]
-    proc = subprocess.run(cmd, capture_output=True, text=True, check=True)
-    assert proc.returncode == 0
-    if verbose:
-        print(f"-- RAW\n{proc.stdout[:300]}\n-- ENDRAW")
-    return map(json.loads, proc.stdout.rstrip().split("\n"))
-
-
-def run_blob(cmd):
-    proc = subprocess.run(
-        cmd, shell=True, capture_output=True, text=True, check=True
-    )  # noqa: E501
-    return proc.stdout
-
-
-def run(cmd):
-    out_blob = run_blob(cmd)
-    out_lines = out_blob.rstrip("\n").split("\n")
-    return out_lines
 
 
 def git_ls_tree(project_path, release="2.0.0"):
@@ -58,7 +32,7 @@ def git_ls_tree(project_path, release="2.0.0"):
 
 def git_tag_list(project_path):
     "list tags (~ releases)"
-    return run(f"git -C {project_path} tag --list")
+    return run.run(f"git -C {project_path} tag --list")
 
 
 # :::::::::::::::::::: APP-CENTRIC FUNCTIONS
@@ -151,7 +125,7 @@ def ls_tags(path):
 @click.argument("path")
 def ctags(path):
     click.echo(f"Ctags {path}")
-    symbols = list(run_ctags(path))
+    symbols = list(run.run_ctags(path))
     pprint.pprint(symbols)
     # run_ctags("../SOURCE/flask/src/flask/app.py", verbose=True)
 
---
+++ april/run.py
@@ -0,0 +1,31 @@
+# run.py
+
+
+import json
+import subprocess
+
+# Universal Ctags
+CTAGS_ARGS = "ctags --output-format=json --fields=*-P -o -".split()
+
+
+def run_ctags(path, verbose=False):
+    "Ctags command output -- iter of dictionaries, one per symbol"
+    cmd = CTAGS_ARGS + [path]
+    proc = subprocess.run(cmd, capture_output=True, text=True, check=True)
+    assert proc.returncode == 0
+    if verbose:
+        print(f"-- RAW\n{proc.stdout[:300]}\n-- ENDRAW")
+    return map(json.loads, proc.stdout.rstrip().split("\n"))
+
+
+def run_blob(cmd):
+    proc = subprocess.run(
+        cmd, shell=True, capture_output=True, text=True, check=True
+    )  # noqa: E501
+    return proc.stdout
+
+
+def run(cmd):
+    out_blob = run_blob(cmd)
+    out_lines = out_blob.rstrip("\n").split("\n")
+    return out_lines
