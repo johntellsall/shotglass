ff1655575581e0697d89acd8d1d0ca770b8e2399
John Mitchell <john@johntellsall.com>
2022-06-09 21:13:45 -0700

    blocks works

--- june/test_spider.py
+++ june/test_spider.py
@@ -4,8 +4,15 @@ from click.testing import CliRunner
 from spider import spider
 
 
-def test_spider_world():
+def test_spider():
     runner = CliRunner()
     result = runner.invoke(spider, ["spider.py"])
+    assert result.exit_code == 0
+    assert "image written" in result.output
+
+
+def test_blocks():
+    runner = CliRunner()
+    result = runner.invoke(spider, ["--style=blocks", "spider.py"])
     assert result.exit_code == 0
     assert "image written" in result.output
