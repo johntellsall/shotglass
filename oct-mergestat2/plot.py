a04cdb2afcc4b3919fdf35bf89bcd7c9f4426978
John Mitchell <john@johntellsall.com>
2022-10-09 10:39:21 -0700

    nice big plot

--- oct-mergestat2/plot.py
+++ oct-mergestat2/plot.py
@@ -3,7 +3,10 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 # Apply the default theme
-sns.set_theme()
+# sns.set_theme()
+sns.set(rc={"figure.dpi": 300, "savefig.dpi": 300})
+sns.set_context("notebook")
+sns.set_style("ticks")
 
 # Load an example dataset
 tips = sns.load_dataset("tips")
