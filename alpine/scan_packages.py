f57f9f4882be760a48a188af4cf97d01e16c62f3
John Mitchell <john@johntellsall.com>
2023-01-16 18:50:59 -0800

    wip: scan

--- alpine/scan_packages.py
+++ alpine/scan_packages.py
@@ -28,13 +28,27 @@ def demo():
     data = get_github_repos("matplotlib/matplotlib")
     pprint.pprint(data)
 
-
+# 
+# CREATE TABLE package_github (
+#     package TEXT, api_repos TEXT);
+# sql: update package_github 
 def main(dbpath):
+    query_github = 'select package, source from alpine where source like "%github.com%"'
+    sql_insert = "insert into package_github (package, api_repos) values (?, ?)"
     with contextlib.closing(sqlite3.connect(dbpath)) as conn:
+        # with conn:
+        #     conn.execute('delete from package_github')
+        assert 0, conn.execute(query_github).fetchall()
         for repos in ["matplotlib/matplotlib"]:
             data = get_github_repos(repos)
             print(f"{repos}: {data['topics']}")
 
+            with conn:
+                conn.execute(sql_insert, ('matplotlib', str(data)))
+
+    with contextlib.closing(sqlite3.connect(dbpath)) as conn:
+        count = list(conn.execute('select count(*) from package_github'))
+    print(f"package_github: {count=}")
 
 if __name__ == "__main__":
     main(sys.argv[1])
