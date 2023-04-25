cd031ce04efcd1cc89cfcabff2ff899e6d30ab0f
John Mitchell <john@johntellsall.com>
2023-04-25 11:50:42 -0700

    save tags to database incrementally vs at end

--- alpine/list-pkg-tags.sql
+++ alpine/list-pkg-tags.sql
@@ -1,14 +1,20 @@
 .schema package_tags
 -- CREATE TABLE package_tags (package TEXT, tag TEXT);
 
+-- NUMBER OF PACKAGES
+select count(distinct package) from package_tags;
+
 -- NUMBER OF PACKAGE_TAGS
 select count(*) from package_tags;
 
 --  SAMPLE
-select * from package_tags limit 3;
+select * from package_tags where package like 'darkhttp%' limit 3;
 
 -- LARGEST PACKAGES
 select package, count(*) from package_tags group by package order by 2 desc limit 5;
+
 -- SMALLEST PACKAGES
 select package, count(*) from package_tags group by package order by 2 asc limit 5;
 
+-- ERRORS FIXME:
+select package, tag from package_tags where tag like "%error%" limit 5;
--- alpine/scan_releases.py
+++ alpine/scan_releases.py
@@ -1,8 +1,9 @@
-# list_tags.py
+# scan_releases.py
 # Per Alpine package, scrape Git tags (= package releases)
 # INPUT:
 # - "alpine" table with package names and source URLs
-# OUTPUT database, "package_tags" table
+# OUTPUT
+# - "package_tags" table
 #
 import contextlib
 import re
@@ -34,6 +35,17 @@ def queryall(conn, sql):
     return conn.execute(sql).fetchall()
 
 
+def save_tags(dbpath, tags):
+    "save the tags to database"
+    sql_insert = "insert into package_tags values (?, ?)"
+    with contextlib.closing(sqlite3.connect(dbpath)) as conn:
+        for package, tags in tags.items():
+            for tag in tags:
+                assert type(tag) is str, "Only strings allowed" # FIXME:
+                conn.execute(sql_insert, (package, tag))
+        conn.commit()
+
+
 def main(dbpath):
     # TODO: note non-GitHub sources
     query_packages = """
@@ -48,36 +60,40 @@ def main(dbpath):
     with contextlib.closing(sqlite3.connect(dbpath)) as conn:
         distro_packages = conn.execute(query_packages).fetchall()
         if not distro_packages:
-            sys.exit("db.alpline: table has no packages")
+            sys.exit("db.alpine: table has no packages")
         cursor = conn.execute("select distinct(package) from package_tags")
         prev_packages = set((row[0] for row in cursor.fetchall()))
 
-    # per package, scrape the list of tags
+    # per package, scrape the list of release tags
     package_tags = {}
     for package, source in distro_packages:
         if package in prev_packages:
             print(f"package={package}: done, skipping")
             continue
+
+        # parse repos URL from package metadata XX
         try:
             repos = repos_pat.search(source).group(1)
         except AttributeError:
             print(f"package={package}: source={source}, repos not found")
+            info = {package: {"error": "repos not found",
+                            source: source }}
+            save_tags(dbpath, info)
             continue
+
+        # get release tags from remote repos (via API)
         tags = list_tags(repos)
         print(f"{package}: {len(tags)}")
         if not tags:
-            # TODO: handle this case; store in db
             print(f"package={package}: tags not found")
+            info = {package: {"error": "tags not found"}}
+            save_tags(dbpath, info)
             continue
         package_tags[package] = tags
 
-    # save the tags to database
-    sql_insert = "insert into package_tags values (?, ?)"
-    with contextlib.closing(sqlite3.connect(dbpath)) as conn:
-        for package, tags in package_tags.items():
-            for tag in tags:
-                conn.execute(sql_insert, (package, tag))
-            conn.commit()
+        # save package's release tags to database
+        save_tags(dbpath, package_tags)
+        package_tags = {}
 
 
 if __name__ == "__main__":
