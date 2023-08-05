a8b79c5cbcc5335dd1d73b8b349d4a95db88e8e3
John Mitchell <john@johntellsall.com>
2023-08-04 21:20:58 -0700

    scan2: put each GitHub Release as separate row in database

--- alpine/gh-releases.sql
+++ alpine/gh-releases.sql
@@ -1,6 +1,9 @@
 -- create table github_releases_blob (
 --     package TEXT, releases_json TEXT
-
+-- create table if not exists github_releases (
+--         package TEXT, -- not unique
+--         release_name TEXT,
+--         release_created_at TEXT -- datetime in ISO 8601
 -- TOTAL PACKAGES
 
 select count(*) from alpine;
@@ -10,3 +13,12 @@ select count(*) from alpine where source like "%github.com%";
 
 -- NUM WITH GITHUB RELEASES
 select count(*) from github_releases_blob;
+
+-- DETAILS: NUM OF PACKAGES
+select count(distinct package) from github_releases;
+-- DETAILS: NUM OF RELEASES
+select count(*) from github_releases;
+
+
+-- SAMPLE RELEASE DETAILS
+select * from github_releases limit 5;
--- alpine/scan2.py
+++ alpine/scan2.py
@@ -5,7 +5,7 @@
 import contextlib
 import json
 import sys
-from dbsetup import dbopen, queryall
+from dbsetup import dbopen, query1, queryall
 import datetime
 
 # parse datetime from this format "2015-01-01T21:15:10Z"
@@ -13,25 +13,60 @@ import datetime
 def parse_datetime(dt_str):
     return datetime.datetime.strptime(dt_str, "%Y-%m-%dT%H:%M:%SZ")
 
+
+def setup(dbpath):
+    "setup database"
+    setup_list = ["""
+    create table if not exists github_releases (
+        package TEXT, -- not unique
+        release_name TEXT,
+        release_created_at TEXT -- datetime in ISO 8601
+                  )
+        """,
+        """delete from github_releases"""  # FIXME: <===
+    ]
+    with contextlib.closing(dbopen(dbpath, readonly=False)) as conn:
+        for sql in setup_list:
+            conn.execute(sql)
+        conn.commit()
+
+
+def add_releases(conn, releases):
+    "add releases to database"
+    sql_insert = "insert into github_releases values (?, ?, ?)"
+    for release in releases:
+        conn.execute(sql_insert, release)
+
+
 def main(dbpath):
+    setup(dbpath)
+
     query = "select package, releases_json from github_releases_blob"
-    query += " where package='jq'"
-    # query += " limit 10"
+    # query += " where package='jq'"
+    # query += " limit 30"
 
     with contextlib.closing(dbopen(dbpath)) as conn:
         package_releases = queryall(conn, query)
 
+    # grab cached GitHub Releases
+    # - this is a list of GH Release details, including name and created_at
     for package, releases_json in package_releases:
         print(package)
-        releases = json.loads(releases_json)
-        if not releases:
+        orig_releases = json.loads(releases_json)
+        if not orig_releases:
             print('- (no releases)')
             continue
-        for release in releases[:5]:
-            print(f'- {release["name"]}')
-            print(f'- {release["created_at"]}')
-            created_at = parse_datetime(release["created_at"])
-            print(f'- {created_at}')
+
+        with contextlib.closing(dbopen(dbpath, readonly=False)) as conn:
+            for release in orig_releases:
+                created_at = parse_datetime(release["created_at"])
+                new_release = (package, release["name"], created_at)
+                add_releases(conn, [new_release])
+            conn.commit()
+
+    with contextlib.closing(dbopen(dbpath)) as conn:
+        num_releases = query1(conn, "select count(*) from github_releases")
+        print(f"{num_releases=}")
 
 
 if __name__ == "__main__":
