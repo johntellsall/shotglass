55a36bc762e10f09b95a536a1879488c4352d65a
John Mitchell <john@johntellsall.com>
2023-08-04 21:56:20 -0700

    wip: plot releases

--- alpine/plot_github_releases.py
+++ alpine/plot_github_releases.py
@@ -7,27 +7,38 @@
 #         release_created_at TEXT -- datetime in ISO 8601
 
 import sqlite3
+import sys
 
 import pandas as pd
 import seaborn as sns
 
 sns.set_theme(style="white")
+label = "github_releases.png"
+
+
+tips = sns.load_dataset("tips")
+print(tips.head())
+plt = sns.relplot(data=tips, x="total_bill", y="tip", hue="day")
+plt.savefig(label)
+sys.exit(0)
+
 
 conn = sqlite3.connect("../shotglass.db")  # FIXME:
 sql_releases = "select package, release_created_at from github_releases limit 3"
 sql_package_release_count = """
-    select package, count(package) 
+    select package, count(package) as release_count
     from github_releases
     group by package
 """
 releases_df = pd.read_sql_query(
     sql_package_release_count, conn
 )
-# assert 0, releases_df.head()
+assert 0, releases_df.head()
 if 0: # FIXME: later
     releases_df['release_created_at'] = pd.to_datetime(releases_df['release_created_at'])
 
 plt = sns.relplot(
+    y="release_count",
     data=releases_df
 )
 # # Plot miles per gallon against horsepower with other semantics
@@ -44,7 +55,5 @@ plt = sns.relplot(
 # )
 
 # # render to file
-import sys
-label = "github_releases.png"
-plt.savefig("demo_scatter.png")
+plt.savefig(label)
 print('DONE')
