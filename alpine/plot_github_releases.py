5de927a68fbd93fc3dcc867e9c63cbb1f8a757dc
John Mitchell <john@johntellsall.com>
2023-08-05 21:09:50 -0700

    plot monthly releases with line average over 3 months

--- alpine/plot_github_releases.py
+++ alpine/plot_github_releases.py
@@ -1,5 +1,6 @@
 # plot_github_releases.py
 # FIXME: in development
+# FIXME: how to note Packages with zero Releases?
 
 # create table if not exists github_releases (
 #         package TEXT, -- not unique
@@ -11,53 +12,45 @@ import sys
 
 import pandas as pd
 import seaborn as sns
+from matplotlib import pyplot
 
 sns.set_theme(style="white")
 label = "github_releases.png"
-
-SAMPLE = False
-
-if SAMPLE:
-    tips = sns.load_dataset("tips")
-    print(tips.head())
-    plt = sns.relplot(data=tips, x="total_bill", y="tip", hue="day")
-    plt.savefig(label)
-    sys.exit(0)
-
-
 conn = sqlite3.connect("../shotglass.db")  # FIXME:
-sql_releases = "select package, release_created_at from github_releases limit 3"
-sql_package_release_count = """
-    select package, count(package) as release_count
-    from github_releases
-    group by package
+
+sql_releases = """
+select
+    strftime('%Y-%m', release_created_at) AS year_month,
+    COUNT(*) AS count
+from github_releases 
+where release_created_at >= '2010-01-01'
+group by year_month
 """
 releases_df = pd.read_sql_query(
-    sql_package_release_count, conn
+    sql_releases, conn
 )
+releases_df['year_month'] = pd.to_datetime(releases_df['year_month'])
+
+print(releases_df.describe())
 print(releases_df.head())
-if 0: # FIXME: later
-    releases_df['release_created_at'] = pd.to_datetime(releases_df['release_created_at'])
+print(releases_df.tail())
 
-releases_df['row_number'] = releases_df.index
-plt = sns.relplot(
-    x="row_number",
-    y="release_count",
-    data=releases_df
-)
-# # Plot miles per gallon against horsepower with other semantics
-# plt = sns.relplot(
-#     x="horsepower",
-#     y="mpg",
-#     hue="origin",
-#     size="weight",
-#     sizes=(40, 400),
-#     alpha=0.5,
-#     palette="muted",
-#     height=6,
-#     data=mpg_df,
-# )
+plt = sns.relplot(x='year_month', y="count", data=releases_df)
 
-# # render to file
+
+# Set 'date' column as the DataFrame index (optional, but can be helpful for time-based resampling)
+releases_df.set_index('year_month', inplace=True)
+
+# Resample the data over 3 months and calculate the mean
+resampled_data = releases_df.resample('3M').mean()
+
+# Create a line plot with x-axis as the resampled date and y-axis as the mean value
+plt.map(sns.lineplot, data=resampled_data, x=resampled_data.index, y='count', color='red')
+# plt.map(sns.lineplot, 'year_month', 'count', color='r', errorbar=None, lw=1)
+
+pyplot.xlabel('Date')
+pyplot.ylabel('Monthly Releases')
+
+# render to file
 plt.savefig(label)
 print('DONE')
