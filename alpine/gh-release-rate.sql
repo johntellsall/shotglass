ae6ce2b31c51d700792c65ed8e031d5f002a4e8d
John Mitchell <john@johntellsall.com>
2023-08-10 10:37:13 -0700

    flag packages with release frequency

--- alpine/gh-release-rate.sql
+++ alpine/gh-release-rate.sql
@@ -57,23 +57,7 @@ select cast(
     having num_releases > 5 and days_per_release >= 90;
 
 
--- XX Middle Released Packages (90+ days per release)
-    select 
-    printf('%-20s', package) as package,
-    strftime('%Y-%m-%d', min(release_created_at)) as first_release,
-    strftime('%Y-%m-%d', max(release_created_at)) as latest_release,
-    count(*) as num_releases,
-
-    cast(
-    julianday(max(release_created_at)) - julianday(min(release_created_at))
-    as integer) / count(*) as days_per_release
-
-    from github_releases
-    group by package
-    having num_releases > 5 and days_per_release between 14+1 and 90-1;
-
-
--- XX Packages tagged by Release Frequency
+-- Packages tagged by Release Frequency
     with BLOB as (
         select
             printf('%-20s', package) as package,
@@ -86,32 +70,14 @@ select cast(
             as integer) / count(*) as days_per_release
         from github_releases
         group by package
-        having num_releases > 5
+        having num_releases >= 5
     )
-    select * from BLOB;
-
-
-
-    -- strftime('%Y-%m-%d', min(release_created_at)) as first_release,
-    -- strftime('%Y-%m-%d', max(release_created_at)) as latest_release,
-    -- count(*) as num_releases,
-
-    -- "biweekly" if 14 days or less
-    -- case when days_per_release <= 14 then 'biweekly'
-    -- else 'OTHER' end as release_frequency 
-
-    -- "monthly" if 30 days or less
-    -- when days_per_release <= 30 then 'monthly'
-    -- -- "quarterly" if 90 days or less
-    -- when days_per_release <= 90 then 'quarterly'
-    -- -- "semiannual" if 180 days or less
-    -- when days_per_release <= 180 then 'semiannual'
-    -- -- "annual" if 365 days or less
-    -- when days_per_release <= 365 then 'annual'
-    -- otherwise "inactive"
-    -- else 'inactive' end as release_frequency 
-
-    -- from github_releases, days_per_release
-    -- group by package
-    -- having num_releases > 5;
+    select *,
+    case when days_per_release <= 14 then 'biweekly'
+    when days_per_release <= 30 then 'monthly'
+    when days_per_release <= 90 then 'quarterly'
+    when days_per_release <= 180 then 'semiannual'
+    when days_per_release <= 365 then 'annual'
+    else 'INACTIVE' end as release_frequency 
+    from BLOB;
 
