b9b5ffeef2b4075a446e4c786dd3bd3144e89f66
John Mitchell <john@johntellsall.com>
2023-08-10 09:39:06 -0700

    calc release stats for single package (and wip: all packages)

--- alpine/gh-release-rate.sql
+++ alpine/gh-release-rate.sql
@@ -1,2 +1,27 @@
--- RELEASE COUNT
+-- create table github_releases (
+--     package TEXT, -- not unique
+--     release_name TEXT,
+--     release_created_at TEXT -- datetime in ISO 8601
+
+-- RELEASE COUNT [tmux]
 select count(*) from github_releases where package='tmux';
+
+-- DATE STATS [tmux]
+select min(release_created_at), max(release_created_at) from github_releases where package='tmux';
+
+-- DATE RANGE [tmux]
+select cast(
+    julianday(max(release_created_at)) - julianday(min(release_created_at))
+    as integer) as days
+from github_releases where package='tmux';
+
+-- AVERAGE DAYS PER RELEASE [tmux]
+select cast(
+    julianday(max(release_created_at)) - julianday(min(release_created_at))
+    as integer) / count(*) as days_per_release
+    from github_releases where package='tmux';
+
+-- DATE STATS (all)
+.mode tabs
+    select package, min(release_created_at), max(release_created_at) from github_releases
+    group by package limit 3;
