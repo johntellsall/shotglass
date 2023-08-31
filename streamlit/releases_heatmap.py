95fcded12dbad69d87bb37475d81bfd2912fdeaa
John Mitchell <john@johntellsall.com>
2023-08-31 11:45:58 -0700

    wip

--- streamlit/releases_heatmap.py
+++ streamlit/releases_heatmap.py
@@ -8,7 +8,7 @@ import altair as alt
 import numpy as np
 import pandas as pd
 
-if 1:
+if 0:
     # Create some example data for a heatmap
     data = pd.DataFrame({
         'x': np.repeat(range(5), 5),
@@ -24,15 +24,23 @@ if 1:
 
 conn = sqlite3.connect("../shotglass.db")  # FIXME:
 
-# strftime('%m', release_created_at) AS month,
 
-# sql_releases = """
-# select
-#     strftime('%Y', release_created_at) AS year,
-#     release_created_at AS release_datetime,
-#     count(*) AS count
-# from github_releases
-# where release_created_at < '2023-08-01' -- FIXME:
-# group by year, month
-# """
-# releases_df = pd.read_sql_query(sql_releases, conn)
+sql_releases = """
+select
+    strftime('%Y', release_created_at) AS year,
+    strftime('%m', release_created_at) AS month,
+    strftime('%Y-%m', release_created_at) AS year_month,
+    release_created_at AS release_datetime,
+    count(*) AS count
+from github_releases
+where release_created_at < '2023-08-01' -- FIXME:
+group by year, month
+"""
+releases_df = pd.read_sql_query(sql_releases, conn)
+
+heatmap = alt.Chart(data).mark_rect().encode(
+        x='x:T',
+        y='y:O',  # Ordinal scale for y-axis
+        color='value:Q'  # Quantitative scale for color
+    )
+st.altair_chart(heatmap, use_container_width=True)
