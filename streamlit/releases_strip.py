51d24cdb684ee78b60ad9a2c049e298283561f41
John Mitchell <john@johntellsall.com>
2023-08-31 20:25:04 -0700

    plot all packages

--- streamlit/releases_strip.py
+++ streamlit/releases_strip.py
@@ -1,4 +1,4 @@
-# releases_strip.py -- show Alpine releases over time as points
+# releases_strip.py -- show Alpine releases over time
 
 import altair as alt
 import streamlit as st
@@ -6,22 +6,6 @@ import streamlit as st
 import sqlite3
 
 import pandas as pd
-import numpy as np
-import pandas as pd
-
-    # Create some example data for a heatmap
-# from vega_datasets import data
-
-# source = data.cars()
-# print(source.info())
-# print(source.columns)
-# # print(source.loc['Horsepower'])
-
-# plot = alt.Chart(source).mark_tick().encode(
-#     x='Weight_in_lbs:Q',
-#     y='Cylinders:O'
-# )
-# st.altair_chart(plot, use_container_width=True)
 
 conn = sqlite3.connect("../shotglass.db")  # FIXME:
 
@@ -30,8 +14,8 @@ select
     package,
     release_created_at
 from github_releases
-where package like 't%' -- FIXME:
 """
+# where package like 't%' -- FIXME:
 data = pd.read_sql_query(sql_releases, conn)
 
 # only "recent", and strip this partial year
@@ -49,25 +33,6 @@ print(data.head())
 # suffix: T=time, Q=quantitative, O=ordinal, N=nominal
 plot = alt.Chart(data).mark_tick().encode(
     y='package:O',
-    x='release_created_at:T',
+    x=alt.X('release_created_at:T', title='Release Date'),
 )
 st.altair_chart(plot, use_container_width=True)
-
-assert 0
-
-# # only render full years worth of data
-# data = data[data.release_datetime < '2023-01-01']  # FIXME:
-
-# data['year'] = pd.to_datetime(data['year'], format='%Y')
-# data['month'] = pd.to_datetime(data['month'], format='%m')
-# print(data.dtypes)
-# # data['month'] = data['month'].dt.strftime('%b')
-
-# print(data.head())
-
-# heatmap = alt.Chart(data).mark_rect().encode(
-#         x='year:T',
-#         y=alt.Y('month:T', sort=None),
-#         color='count:Q'  # Quantitative scale for color
-#     )
-# st.altair_chart(heatmap, use_container_width=True)
