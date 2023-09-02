f5e284a7efcb912a30684d4d91766c4d2d3d80d8
John Mitchell <john@johntellsall.com>
2023-09-02 13:13:14 -0700

    clean for release

--- streamlit/releases_strip.py
+++ streamlit/releases_strip.py
@@ -15,19 +15,14 @@ select
     release_created_at
 from github_releases
 """
-# where package like 't%' -- FIXME:
 data = pd.read_sql_query(sql_releases, conn)
 
 # only "recent", and strip this partial year
 data['release_created_at'] = pd.to_datetime(data['release_created_at'])
 data = data[data.release_created_at >= '2010-01-01']
-data = data[data.release_created_at < '2023-01-01']
+data = data[data.release_created_at < '2023-01-01']  # FIXME:
 
 print(data.describe())
-
-# Reset the index and treat it as a data column
-data.reset_index(drop=True, inplace=True)
-
 print(data.head())
 
 # suffix: T=time, Q=quantitative, O=ordinal, N=nominal
