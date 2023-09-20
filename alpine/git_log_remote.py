9e35dfd476d71fc8ceb78e015a64b8dbaef8a643
John Mitchell <john@johntellsall.com>
2023-09-19 21:01:44 -0700

    fetch remote Git tags and show info

--- alpine/git_log_remote.py
+++ alpine/git_log_remote.py
@@ -1,36 +1,39 @@
 # git_log_remote.py -- list tags and info from remote git repo
 # Supports all Git repos, not just GitHub
 
+import pathlib
 import sys
 import subprocess
+import tempfile
 
 
 def run(args):
     return subprocess.check_output(args).decode('utf-8')
 
-import tempfile
-
-
+# FIXME: create new tempdir if cache is for different repos
+# - check git_url inside FETCH_HEAD header
 def get_tag_info(git_url, datadir):
-    # FIXME: only fetch if needed
+    has_cache = pathlib.Path(datadir, "HEAD").exists()
     temp_gitdir = f'--git-dir={datadir}'
-    run(['git', temp_gitdir, 'init'])
-    out = run(['git', temp_gitdir, 'fetch', '--tags', '--prune', git_url]) 
-    print(out)
-    # out = run(['git', temp_gitdir, 'tag', '-l'])
-    out = run(['git', temp_gitdir, 'tag', '-l', '--format="%(refname:short) %(objectname:short) %(subject)"'])
-    print(out)
-# 	git --git-dir=/tmp/tmux log --date=iso --pretty=format:'%ad %S %s' \
-
+    if not has_cache:
+        run(['git', temp_gitdir, 'init'])
+        out = run(['git', temp_gitdir, 'fetch', '--tags', '--prune', git_url]) 
+        print(out)
+    tag_format = '--format="%(refname:short) %(*authordate) %(subject)"'
+    out = run(['git', temp_gitdir, 'tag', '-l', tag_format])
     return out
 
-# FIXME: create and teardown tempdir
+
 def main(repos_url):
     print(repos_url)
-    tmpdir = '/tmp/test_remote'
-    # with tempfile.TemporaryDirectory() as tmpdir:
-    get_tag_info(repos_url, tmpdir)
-        # print(out)
+    cached = False
+    if cached:
+        tmpdir = '/tmp/test_remote'
+        tag_lines = get_tag_info(repos_url, tmpdir)
+    else:
+        with tempfile.TemporaryDirectory() as tmpdir:
+            tag_lines = get_tag_info(repos_url, tmpdir)
+    print(tag_lines)
 
 # 	git --git-dir=/tmp/tmux log --date=iso --pretty=format:'%ad %S %s' \
 # 		$$(cat z)
