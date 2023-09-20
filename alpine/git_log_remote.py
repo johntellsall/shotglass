9d3dbdd546d77d8bd709e3cc21b73702cb8fa57f
John Mitchell <john@johntellsall.com>
2023-09-19 20:34:31 -0700

    init, fetch, list tags

--- alpine/git_log_remote.py
+++ alpine/git_log_remote.py
@@ -1,16 +1,26 @@
+# git_log_remote.py -- list tags and info from remote git repo
+# Supports all Git repos, not just GitHub
 
 import sys
+import subprocess
 
 
+def run(args):
+    return subprocess.check_output(args).decode('utf-8')
+
+
+# FIXME: create and teardown tempdir
 def main(repos_url):
     print(repos_url)
-# git --git-dir=/tmp/tmux init
-# 	git --git-dir=/tmp/tmux fetch --tags --prune \
-# 		https://github.com/tmux/tmux
-# 	git --git-dir=/tmp/tmux tag -l
-# git --git-dir=/tmp/tmux tag -l > z
+    run(['git', '--git-dir=/tmp/tmux', 'init'])
+    out = run(['git', '--git-dir=/tmp/tmux', 'fetch', '--tags', '--prune', repos_url]) 
+    print(out)
+    out = run(['git', '--git-dir=/tmp/tmux', 'tag', '-l'])
+    print(out)
+
 # 	git --git-dir=/tmp/tmux log --date=iso --pretty=format:'%ad %S %s' \
 # 		$$(cat z)
+
 
 if __name__=='__main__':
     main(sys.argv[1])
