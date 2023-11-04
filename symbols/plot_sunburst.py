e1962e365a26cdd092925e235f498749b91c4fa6
John Mitchell <john@johntellsall.com>
2023-11-03 18:02:59 -0700

    sunburst plot works

--- symbols/plot_sunburst.py
+++ symbols/plot_sunburst.py
@@ -1,16 +1,30 @@
 import plotly.express as px
 import pandas as pd
 import state
+from pathlib import Path
 
 db = state.get_db()
 
 paths_df = pd.read_sql_query("select distinct(path) from symbol", db)
-print(paths_df.head())
 
 # remove boring paths
 paths_df = paths_df[~paths_df['path'].str.contains('__init__')]
 # TODO: loop and remove other boring directories
-print(paths_df.head())
 
-# df = px.data.tips()
+data = []
+for index, row in paths_df.iterrows():
+    parts = Path(row['path']).parts
+    if parts[:2] == ('lib', 'sqlalchemy'):
+        parts = tuple(parts[2:])
+    data.append(parts)
+
+plot_df = pd.DataFrame(data)
+print(paths_df.head())
+print(paths_df.describe())
+
+print(plot_df.head())
+print(plot_df.describe())
+
 # fig = px.sunburst(df, path=['day', 'time', 'sex'], values='total_bill')
+fig = px.sunburst(plot_df, path=list(range(3)))
+fig.show()
