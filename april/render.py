2390ebcb34eac3bf1b9440c5e193a1ee13f1981c
John Mitchell <john@johntellsall.com>
2024-05-28 10:50:46 -0700

    plot symbol sizes

--- april/render.py
+++ april/render.py
@@ -1,33 +1,61 @@
 # render.py
 
 import sqlite3
+from math import sqrt
+from itertools import islice
 
 # FIXME: define xy as list of two numbers
 class Cursor:
     def __init__(self, width):
         self.xy = [0, 0]
-        self.width = width
+        self.width = int(width)
 
     def skip(self, num):
-        self.xy[0] += num
+        self.xy[0] += int(num)
         while self.xy[0] >= self.width:
-            print(self.xy)
             self.xy[0] -= self.width
             self.xy[1] += 1
-        print(f'-> {self.xy}')
        
 
-
 def dbopen():
-    return sqlite3.connect('shotglass.db')
+    conn = sqlite3.connect('shotglass.db')
+    conn.row_factory = sqlite3.Row
+    return conn
 
-# select first three items from tag table
-SQL_LIST_TAGS = '''
-select path,name,line,end from tag
+# TODO: strip small tags?
+
+SQL_TOTAL = '''
+select sum(end - line) as total
+from tag
 where end != 'UNKNOWN-end'
-limit 3
+'''
+
+SQL_LIST_TAGS = '''
+select path,name,line,end,
+end - line AS size
+from tag
+where end != 'UNKNOWN-end'
 '''
 def render():
+    sql = SQL_LIST_TAGS
+    sql += ' and size >= 9' # limit to larger tags
+
     with dbopen() as conn:
-        for (tagpath, name, start, end) in conn.execute(SQL_LIST_TAGS):
-            print(f"{start}, {end} {name} {tagpath}")
+        total, image_size = get_total_lines(conn)
+        print(f'{total} LOC, {image_size=}')
+        cursor = Cursor(image_size)
+        rows = conn.execute(sql)
+        if 1:
+            rows = islice(rows, 3)
+        for row in rows:
+            print(cursor.xy, end=' ')
+            print('{size}\t{name} {path}'.format(**row))
+            cursor.skip(row['size'])
+    print(f"{cursor.xy} end")
+
+def get_total_lines(conn):
+    res = conn.execute(SQL_TOTAL)
+    total = res.fetchone()['total']
+    image_size = 1 + int(sqrt(total))
+    return total,image_size
+    
