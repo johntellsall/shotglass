6338bb3693ffee27dd70cd943193f106ab9e617b
John Mitchell <john@johntellsall.com>
2024-05-29 10:35:08 -0700

    cursor now 100% complete

--- april/cursor.py
+++ april/cursor.py
@@ -7,27 +7,21 @@ class Cursor:
 
     def skip(self, num):
         """emit slices of consumed area
-        Note: slice endpoints are *exclusive*
-        Ex: 3-wide row, consume 2:
-        - start at 0,0
-        - move to 2,0 -- two spots consumed
-        -> slice is ([0,0], [2,0]) -- end is exclusive
-        -> draw [0,0] and [1,0] (two spots) don't draw [2,0]
-        FIXME: slices are lists of tuples, for PIL.ImageDraw
-        """
+         """
         slices = []
-        if num + self.xy[0] <= self.width:
+        while num + self.xy[0] >= self.width:
+            # consume full row
+            old = tuple(self.xy)
+            num -= self.width - self.xy[0]
+            slice = [tuple(self.xy), (self.width, self.xy[1])]
+            slices.append(slice)
+            # move down one row
+            self.xy = [0, self.xy[1] + 1]
+
+        if num > 0 and num + self.xy[0] <= self.width:
             old = tuple(self.xy)
             self.xy[0] += num
             slices.append([old, tuple(self.xy)])
-        # else:
-        #     # consume full row
-        #     end = self.xy[0] + self.width
-        #     slices.append((self.xy, [end, self.xy[1]] )
-        #     self.xy = [0, self.xy[1] + 1]
-        #     # consume rest of row
-        #     end = self.xy[0] + num - self.width
-        #     slices.append((self.xy, [end, self.xy[1]] )
-        #     self.xy = [end, self.xy[1]]
+
         return slices
        
--- april/test_cursor.py
+++ april/test_cursor.py
@@ -1,5 +1,13 @@
 from cursor import Cursor
 
+#  Note: slice endpoints are *exclusive*
+# Example: 3-wide row, consume 2:
+# - start at 0,0
+# - move to 2,0 -- two spots consumed
+# -> slice is ([0,0], [2,0]) -- end is exclusive
+# -> draw [0,0] and [1,0] (two spots) don't draw [2,0]
+# - next spot is [2,0]
+      
 def test_same_row():
     "move in same row"
     cursor = Cursor(3)
@@ -8,32 +16,20 @@ def test_same_row():
     assert cursor.xy == [2, 0]
     assert slices == [[(0, 0), (2, 0)]]
 
-def test_cursor2():
+def test_next_row():
     "jump to next row"
     cursor = Cursor(3)
-    cursor.skip(3)
+    slices = cursor.skip(3)
     assert cursor.xy == [0, 1]
+    assert slices == [[(0, 0), (3, 0)]]
 
-def test_cursor_slice1():
-    "move in same row"
-    slices = Cursor(3).skip(2)
-    assert slices == [([0, 0], [2, 0])]
-
-
-# def test_cursor_slice3():
-#     "jump to next row: slice is same
-#     slices = Cursor(3).skip(3)
-#     assert slices == [([0, 0], [2, 0])]
-   
-
-# def test_cursor_slice2():
-#     "move to next row"
-#     # a 5-wide tag will consume one full row (width=3) and 2 of 2nd row
-#     slices = Cursor(3).skip(5)
-#     # 5-wide tag:
-#     # - start at upper left (0,0)
-#     # - move to upper right (2,0)
-#     # - start at 2nd row left again (0,1)
-#     # - move two steps to the right (2,1)
-#     assert slices[0] == ([0, 0], [2, 0]) # first row: full
-#     assert ([0, 1], [2, 1])
+def test_next_row_and_col():
+    "move in X and Y"
+    cursor = Cursor(3)
+    slices = cursor.skip(5)
+    # first row: all 3 spots consumed
+    assert slices.pop(0) == [(0, 0), (3, 0)]
+    # second row: 2 spots consumed
+    assert slices.pop(0) == [(0, 1), (2, 1)]
+    assert not slices
+    assert cursor.xy == [2, 1]
