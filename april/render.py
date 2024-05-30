cfbd93b964585940330a79edadd2eb4a58b1a590
John Mitchell <john@johntellsall.com>
2024-05-30 15:07:53 -0700

    render *files* as squares in different colors

--- april/render.py
+++ april/render.py
@@ -31,7 +31,7 @@ def get_path_numlines(conn):
     sql = SQL_PATH_NUMLINES
     return conn.execute(sql)
 
-def get_image_size(num_lines):
+def calc_image_size(num_lines):
     return 1 + int(sqrt(num_lines))
 
 def get_colors(count):
@@ -55,6 +55,8 @@ def render_tags(image, tags):
             color_num += 1
             draw.line(slice, fill=color, width=1)
 
+from rectpack import newPacker
+
 def render():
     sql = SQL_LIST_TAGS
     if 1:
@@ -62,19 +64,48 @@ def render():
 
     with dbopen() as conn:
         path_numlines = list(get_path_numlines(conn))
-        total_numlines = sum(d['numlines'] for d in path_numlines)
-        assert 0, total_numlines
-        # assert 0, iter(path_numlines)
-        for temp in path_numlines:
-            print(dict(temp))
-        # print(f'{total} LOC, {image_size=}')
-        tags = conn.execute(sql)
 
-    if 0:
-        tags = islice(tags, 3)
+    total_numlines = sum(info['numlines'] for info in path_numlines)
+    image_size = calc_image_size(total_numlines)
+    print(f'{total_numlines} LOC, {image_size=}')
 
     image = Image.new('RGB', (image_size, image_size), color='gray')
-    render_tags(image, tags)
+    rectangles = []
+    for info in path_numlines:
+        # print(info)
+        # assert 0, dict(info)
+        box_size = calc_image_size(info['numlines'])
+        rectangles.append((box_size, box_size))
+    bins = [(image_size, image_size)]
+
+    packer = newPacker()
+    for r in rectangles:
+        packer.add_rect(*r)
+
+    # Add the bins where the rectangles will be placed
+    for b in bins:
+        packer.add_bin(*b)
+    packer.pack()
+
+    draw = ImageDraw.Draw(image)
+    colors = list(get_colors(8))
+    color_num = 0
+    # TODO: pay attention to 2nd bin
+    for rect in packer[0].rect_list():
+        x, y, w, h, _rid = rect
+        color = colors[color_num % len(colors)]
+        color_num += 1
+        draw.rectangle((x, y, x+w, y+h), fill=color)
+    # assert 0, iter(path_numlines)
+        # for temp in path_numlines:
+        #     print(dict(temp))
+        # # print(f'{total} LOC, {image_size=}')
+        # tags = conn.execute(sql)
+
+    # if 0:
+    #     tags = islice(tags, 3)
+
+    # render_tags(image, tags)
 
     image.show()
     image.save('out.png')
