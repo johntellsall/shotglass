c9151d5c62f928a062d45d48fa385c7371ae3309
John Mitchell <johnlmitchell@gmail.com>
2025-01-01 09:28:58 -0800

    summarize packages

--- 2025/main.py
+++ 2025/main.py
@@ -3,14 +3,26 @@ from pprint import pprint
 import sys
 from parse import parse
 
-def main(args):
+def show_info(args):
     for topdir in args:
         print(topdir)
         path = Path(topdir) / 'APKBUILD'
-
         info = parse(open(path))
         pprint(info)
         print()
 
+def show_summary(args):
+    fields = ['pkgname', 'pkgver', 'pkgrel', 'len_install', 'len_parse_funcs', 'len_subpackages']
+    print(fields)
+    for topdir in args:
+        path = Path(topdir) / 'APKBUILD'
+        info = parse(open(path))
+        #'pkgdesc'
+        info['len_install'] = len(info.get('install', []))
+        info['len_parse_funcs'] = len(info.get('_parse_functions', []))
+        info['len_subpackages'] = len(info.get('subpackages', []))
+        # print(list(zip(fields, [info.get(f, 'N/A') for f in fields])))
+        print([info.get(f, 'N/A') for f in fields])
+
 if __name__ == '__main__':
-    main(sys.argv[1:])
+    show_summary(sys.argv[1:])
