d2d8bedd95ff630c77627412205c66a7dd411d21
John Mitchell <johnlmitchell@gmail.com>
2025-01-02 10:20:08 -0800

    ignore deleting nonexistent tables

--- 2025/extract.py
+++ 2025/extract.py
@@ -6,16 +6,20 @@ from sqlmodel import select, delete, func, Field, Session, SQLModel, create_engi
 from model import SGAlpinePackage
 from parse import parse
 from lib import get_engine
+from sqlalchemy.exc import OperationalError
 
 
 def extract(paths):
     engine = get_engine()
 
     # WARNING: Remove all existing SGAlpinePackage entries
-    with Session(engine) as session:
-        statement = delete(SGAlpinePackage)
-        session.exec(statement)
-        session.commit()
+    try:
+        with Session(engine) as session:
+            statement = delete(SGAlpinePackage)
+            session.exec(statement)
+            session.commit()
+    except OperationalError as e:
+        print(f"OperationalError: {e}") # FIXME:
 
     SQLModel.metadata.create_all(engine)
 
