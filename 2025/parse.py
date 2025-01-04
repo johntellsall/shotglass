fa9444bed6bd39b46aab612dd0fa333d7f70acd2
John Mitchell <johnlmitchell@gmail.com>
2025-01-04 12:50:26 -0800

    parse debian popcon

--- 2025/parse.py
+++ 2025/parse.py
@@ -40,3 +40,31 @@ def parse(lines, label=None):
             continue
 
     return info
+
+
+def parse_debian_popcon(data):
+    # Skip comment lines and prepare data for parsing
+    lines = [line for line in data.splitlines() if line and not line.startswith('#')]
+    
+    # Create a list to store results
+    packages = []
+    
+    # Parse each line
+    for line in lines:
+        # Split on variable whitespace
+        fields = [field for field in line.split() if field]
+        
+        # Create a dictionary for each package
+        package = {
+            'rank': int(fields[0]),
+            'name': fields[1],
+            'inst': int(fields[2]),
+            'vote': int(fields[3]),
+            'old': int(fields[4]),
+            'recent': int(fields[5]),
+            'no_files': int(fields[6]),
+            'maintainer': ' '.join(fields[7:]).strip('()')  # Join remaining fields and remove parentheses
+        }
+        packages.append(package)
+    
+    return packages
--- 2025/test_parse.py
+++ 2025/test_parse.py
@@ -1,27 +1,27 @@
 import pprint
-from parse import parse
+import parse
 import pytest
 
 LINES = list(open('APKBUILD').readlines())
 
 
 def test_simple():
-    info = parse(LINES)
+    info = parse.parse(LINES)
     assert info['pkgname'] == 'dnsmasq'
     assert info['pkgver'] == '2.90'
 
 def test_string():
-    info = parse(LINES)
+    info = parse.parse(LINES)
     assert info['pkgdesc'] == "A lightweight DNS, DHCP, RA, TFTP and PXE server"
 
 def test_multiline():
-    info = parse(LINES)
+    info = parse.parse(LINES)
     value = info['makedepends']
     assert value[:2] == ['coreutils', 'dbus-dev']
     assert value[-2:] == ['nettle-dev', 'nftables-dev']
 
 def test_function():
-    info = parse(LINES)
+    info = parse.parse(LINES)
     assert(info['_parse_functions'] == ['build', 'check', 'package', 'dnssec', 'nftset', 'dbus', 'common', 'openrc', 'utils', 'utils_doc'])
 
 # def test_alt():
@@ -34,3 +34,15 @@ def test_list_quote():
     lines = open('test-data/list-quote.dat')
     info = parse(lines)
     assert 0, pprint.pformat(info)
+
+
+def test_parse_popcon():
+    data = '''#<no-files> is the number of people whose entry didn't contain enough
+#           information (atime and ctime were 0).
+#rank name                            inst  vote   old recent no-files (maintainer)
+1     libacl1                        138898 126117     2 12764    15 (Guillem Jover)                 
+2     libpcre2-8-0                   138890 126112     3 12762    13 (Matthew Vernon)'''
+
+    packages = parse.parse_debian_popcon(data)
+    assert packages[0] == {'rank': 1, 'name': 'libacl1', 'inst': 138898, 'vote': 126117, 'old': 2, 'recent': 12764, 'no_files': 15, 'maintainer': 'Guillem Jover'}
+    assert len(packages) == 2
