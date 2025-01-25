480465003b3add178cb15485c2efc9d14b0e152a
John Mitchell <johnlmitchell@gmail.com>
2025-01-24 22:41:31 -0800

    release-packages over time; 3 releases

--- 2025/r_grid.py
+++ 2025/r_grid.py
@@ -1,13 +1,15 @@
+from collections import defaultdict
+from pprint import pprint
 import sys
 import matplotlib.pyplot as plt
 import numpy as np
 from lib import equery
 
 
-vegetables = ["cucumber", "tomato", "lettuce", "asparagus",
-              "potato", "wheat", "barley"]
-farmers = ["Joe", "Bros.", "Smith",
-           "Agrifun", "Organic", "Ltd.", "Corp."]
+# vegetables = ["cucumber", "tomato", "lettuce", "asparagus",
+#               "potato", "wheat", "barley"]
+# farmers = ["Joe", "Bros.", "Smith",
+#            "Agrifun", "Organic", "Ltd.", "Corp."]
 
 harvest = np.array([[0.8, 2.4, 2.5, 3.9, 0.0, 4.0, 0.0],
                     [2.4, 0.0, 4.0, 1.0, 2.7, 0.0, 0.0],
@@ -19,17 +21,17 @@ harvest = np.array([[0.8, 2.4, 2.5, 3.9, 0.0, 4.0, 0.0],
 
 title = "Package Versions over Time"
 
-sql = """
-SELECT a.alpine_release, a.pkgname, a.pkgver, a.pkgrel, d.rank
-FROM sgalpinepackage a 
-JOIN debianpopcontest d 
-ON a.pkgname = d.name 
-WHERE a.alpine_release in ('3.0-stable', '3.10-stable', '3.21-stable')
-AND d.rank <= 50
--- remove Debian-isms
-AND a.pkgname not in ('dpkg', 'debian-archive-keyring', 'debootstrap')
-ORDER BY a.alpine_release DESC, d.rank;
-"""
+# sql = """
+# SELECT a.alpine_release, a.pkgname, a.pkgver, a.pkgrel, d.rank
+# FROM sgalpinepackage a 
+# JOIN debianpopcontest d 
+# ON a.pkgname = d.name 
+# WHERE a.alpine_release in ('3.0-stable', '3.10-stable', '3.21-stable')
+# AND d.rank <= 50
+# -- remove Debian-isms
+# AND a.pkgname not in ('dpkg', 'debian-archive-keyring', 'debootstrap')
+# ORDER BY a.alpine_release DESC, d.rank;
+# """
 
 sql = """
 SELECT a.alpine_release, a.pkgname, a.pkgver
@@ -40,16 +42,30 @@ WHERE a.alpine_release in ('3.0-stable', '3.10-stable', '3.21-stable')
 AND d.rank <= 500
 AND a.pkgname not in ('dash', 'dpkg', 'debian-archive-keyring', 'debootstrap')
 ORDER BY a.alpine_release DESC, d.rank
-LIMIT 5
 """
 
 def grid():
 
     data = equery(sql)
     print(data)
-    sys.exit('DING')
-    print(len(data))
-    breakpoint()
+
+    # X: Alpine releases
+    releases = list(set(row[0] for row in data))
+    print(releases)
+
+    # Y: package names
+    # - order: earlier = higher popularity rank
+    pkgnames = list(set(row[1] for row in data))
+
+    pkgnames = pkgnames[:7] # FIXME:
+    print(pkgnames)
+
+    info = {}
+    for rel, pkgname, pkgver in data:
+        if pkgname in pkgnames:
+            info[rel, pkgname] = pkgver
+
+    pprint(info)
 
     fig, ax = plt.subplots()
     _im = ax.imshow(harvest)
@@ -57,17 +73,19 @@ def grid():
     xt_style = dict(rotation=45, ha="right", rotation_mode="anchor")
 
     # Show all ticks and label them with the respective list entries
-    ax.set_xticks(range(len(farmers)), labels=farmers, **xt_style)
-    ax.set_yticks(range(len(vegetables)), labels=vegetables)
+    short_rel = [rel.split('-')[0] for rel in releases]
+    ax.set_xticks(range(len(releases)), labels=short_rel, **xt_style)
+    ax.set_yticks(range(len(pkgnames)), labels=pkgnames)
 
     t_style = dict(ha="center", va="center", color="white", fontweight="bold")
 
     # Loop over data dimensions and create text annotations.
-    for i in range(len(vegetables)):
-        for j in range(len(farmers)):
-            text = ax.text(j, i, harvest[i, j], **t_style)
+    for pi,pkgname in enumerate(pkgnames):
+        for ri,release in enumerate(releases):
+            pkg_version = info.get((release, pkgname), '-')
+            text = ax.text(x=ri, y=pi, s=pkg_version, **t_style)
 
-    ax.set_title(title)
+    ax.set_title("test 1020")
     fig.tight_layout()
     plt.savefig("beer.png")
     print('DING')
