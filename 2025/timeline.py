aba51a13099ab4354459d639eb19d2e1dcc7d0a6
John Mitchell <johnlmitchell@gmail.com>
2025-02-16 10:40:08 -0800

    make general get package info function

--- 2025/timeline.py
+++ 2025/timeline.py
@@ -10,20 +10,18 @@ we show how to create a simple timeline using the dates for recent releases
 of Matplotlib. First, we'll pull the data from GitHub.
 """
 
+import json
+import urllib.request
 from datetime import datetime
 
 import matplotlib.pyplot as plt
 import numpy as np
-
 import matplotlib.dates as mdates
 
-try:
-    # Try to fetch a list of Matplotlib releases and their dates
-    # from https://api.github.com/repos/matplotlib/matplotlib/releases
-    import json
-    import urllib.request
 
-    url = 'https://api.github.com/repos/matplotlib/matplotlib/releases'
+def get_package_releases(package_name):
+    # url = 'https://api.github.com/repos/matplotlib/matplotlib/releases'
+    url = f'https://api.github.com/repos/{package_name}/releases'
     url += '?per_page=100'
     data = json.loads(urllib.request.urlopen(url, timeout=1).read().decode())
 
@@ -33,20 +31,29 @@ try:
         if 'rc' not in item['tag_name'] and 'b' not in item['tag_name']:
             dates.append(item['published_at'].split("T")[0])
             releases.append(item['tag_name'].lstrip("v"))
+    
+    tag_names = [ item['tag_name'] for item in data]
+    info = dict(tag_names=tag_names, dates=dates, releases=releases)
+    return info
 
-except Exception:
-    # In case the above fails, e.g. because of missing internet connection
-    # use the following lists as fallback.
-    releases = ['2.2.4', '3.0.3', '3.0.2', '3.0.1', '3.0.0', '2.2.3',
-                '2.2.2', '2.2.1', '2.2.0', '2.1.2', '2.1.1', '2.1.0',
-                '2.0.2', '2.0.1', '2.0.0', '1.5.3', '1.5.2', '1.5.1',
-                '1.5.0', '1.4.3', '1.4.2', '1.4.1', '1.4.0']
-    dates = ['2019-02-26', '2019-02-26', '2018-11-10', '2018-11-10',
-             '2018-09-18', '2018-08-10', '2018-03-17', '2018-03-16',
-             '2018-03-06', '2018-01-18', '2017-12-10', '2017-10-07',
-             '2017-05-10', '2017-05-02', '2017-01-17', '2016-09-09',
-             '2016-07-03', '2016-01-10', '2015-10-29', '2015-02-16',
-             '2014-10-26', '2014-10-18', '2014-08-26']
+pkg_info = get_package_releases('matplotlib/matplotlib')
+releases = pkg_info['releases']
+dates = pkg_info['dates']
+
+
+
+    # # In case the above fails, e.g. because of missing internet connection
+    # # use the following lists as fallback.
+    # releases = ['2.2.4', '3.0.3', '3.0.2', '3.0.1', '3.0.0', '2.2.3',
+    #             '2.2.2', '2.2.1', '2.2.0', '2.1.2', '2.1.1', '2.1.0',
+    #             '2.0.2', '2.0.1', '2.0.0', '1.5.3', '1.5.2', '1.5.1',
+    #             '1.5.0', '1.4.3', '1.4.2', '1.4.1', '1.4.0']
+    # dates = ['2019-02-26', '2019-02-26', '2018-11-10', '2018-11-10',
+    #          '2018-09-18', '2018-08-10', '2018-03-17', '2018-03-16',
+    #          '2018-03-06', '2018-01-18', '2017-12-10', '2017-10-07',
+    #          '2017-05-10', '2017-05-02', '2017-01-17', '2016-09-09',
+    #          '2016-07-03', '2016-01-10', '2015-10-29', '2015-02-16',
+    #          '2014-10-26', '2014-10-18', '2014-08-26']
 
 dates = [datetime.strptime(d, "%Y-%m-%d") for d in dates]  # Convert strs to dates.
 releases = [tuple(release.split('.')) for release in releases]  # Split by component.
@@ -114,23 +121,3 @@ ax.spines[["left", "top", "right"]].set_visible(False)
 ax.margins(y=0.1)
 plt.savefig("timeline.png")
 
-
-# %%
-#
-# .. admonition:: References
-#
-#    The use of the following functions, methods, classes and modules is shown
-#    in this example:
-#
-#    - `matplotlib.axes.Axes.annotate`
-#    - `matplotlib.axes.Axes.vlines`
-#    - `matplotlib.axis.Axis.set_major_locator`
-#    - `matplotlib.axis.Axis.set_major_formatter`
-#    - `matplotlib.dates.MonthLocator`
-#    - `matplotlib.dates.DateFormatter`
-#
-# .. tags::
-#
-#    component: annotate
-#    plot-type: line
-#    level: intermediate
