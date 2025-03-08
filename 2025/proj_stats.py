5bb46390f9d8ffaddd5d6325b693c0b32cbc6f6b
John Mitchell <john@johntellsall.com>
2025-03-07 20:33:53 -0800

    wip

--- 2025/proj_stats.py
+++ 2025/proj_stats.py
@@ -4,6 +4,9 @@ from dataclasses import dataclass
 from pathlib import Path
 
 
+# NOTE: yaml?
+SOURCE_SUFFIXES = ('.c', '.h', '.py', '.sh')
+
 @dataclass
 class Project:
     name: str
@@ -22,10 +25,19 @@ def proj_stats(topdir):
     for path in top.rglob('*'):
         if path.is_dir():
             proj.dirs.add(path)
+            continue
+        if path.suffix in SOURCE_SUFFIXES:
+            proj.source_files.add(path)
 
     print(f'PROJECT: {proj.name}')
     print(f'  Directories: {len(proj.dirs)}')
     print(f'  Source files: {len(proj.source_files)}')
+
+    segmets = set()
+    for pdir in proj.dirs:
+        segmets.update(pdir.parts)
+    print(f'  Segments: {len(segmets)}')
+    print( sorted(list(segmets)) )
 
 def main(paths):
     for path in paths:
