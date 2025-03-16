e917b4379e8f939d8211ab368c104c5a715f0594
John Mitchell <johnlmitchell@gmail.com>
2025-03-15 19:09:29 -0700

    alpine stackplot

--- 2025/alpine_stackplot.py
+++ 2025/alpine_stackplot.py
@@ -11,22 +11,31 @@ plt.style.use('_mpl-gallery')
 
 releases = lib.equery_col('select distinct(alpine_release) from sgalpinepackage')
 releases.sort(key=lib.cmp_version)
-assert 0, releases
 
+num_packages_per_release = lib.equery('''
+select alpine_release, count(*)
+from sgalpinepackage
+group by alpine_release
+''')
+# assert 0, num_packages_per_release
 # make data
-x = np.arange(0, 10, 2)
-ay = [1, 1.25, 2, 2.75, 3]
-by = [1, 1, 1, 1, 1]
-cy = [2, 1, 2, 1, 2]
-y = np.vstack([ay, by, cy])
+# x = np.arange(0, 10, 2)
+x = releases
+if 0:
+       ay = [1, 1.25, 2]
+       by = [1, 1, 1]
+       cy = [2, 1, 2]
+       y = np.vstack([ay, by, cy])
+else:
+       y = [row[1] for row in num_packages_per_release]
 
 # plot
 fig, ax = plt.subplots()
 
 ax.stackplot(x, y)
 
-ax.set(xlim=(0, 8), xticks=np.arange(1, 8),
-       ylim=(0, 8), yticks=np.arange(1, 8))
+# ax.set(xlim=(0, 8), xticks=np.arange(1, 8),
+#        ylim=(0, 8), yticks=np.arange(1, 8))
 
 # plt.show()
 page_path = lib.savefig(plt, __file__)
