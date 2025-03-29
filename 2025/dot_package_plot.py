9e085f7ad0a2f177f2afecf4fccd6064168c375c
John Mitchell <johnlmitchell@gmail.com>
2025-03-29 12:15:58 -0700

    add legend

--- 2025/dot_package_plot.py
+++ 2025/dot_package_plot.py
@@ -44,7 +44,7 @@ def get_size(sizedb, release, pkgname):
     assert '-' not in release
     if info := sizedb.get(release, {}).get(pkgname):
         return calc_size(info['size'])
-    print(f'{release} {pkgname} not found in size db')
+    # print(f'{release} {pkgname} not found in size db')
     global output_size_notfound
     output_size_notfound += 1
     return 0
@@ -56,7 +56,8 @@ def parse_release(verstring):
 
 
 
-plt.style.use('_mpl-gallery')
+if 0:
+    plt.style.use('_mpl-gallery')
 
 
 res = equery('''
@@ -85,8 +86,6 @@ def update_size_cache(releases):
 releases = set(parse_release(info[0]) for info in res)
 size_db = update_size_cache(releases)
 
-# pkgnames = [info[1] for info in res]
-
 x = [item[2] for item in res]
 y = [get_size(size_db, release=parse_release(item[0]), pkgname=item[1]) for item in res]
 
@@ -107,11 +106,24 @@ colors = [float(parse_release(info[0])) for info in res]
 # plot
 fig, ax = plt.subplots()
 
-# ax.scatter(x, y, s=sizes, c=colors, vmin=3.7, vmax=4.0)
-ax.scatter(x, y, c=colors, edgecolor=edges)
+if 0:
+    ax.scatter(x, y, c=colors, edgecolor=edges)
+else:
+    for rel in releases:
+        rel_x = [x[i] for i in range(len(x)) if parse_release(res[i][0]) == rel]
+        rel_y = [y[i] for i in range(len(y)) if parse_release(res[i][0]) == rel]
+        ax.scatter(rel_x, rel_y, label=rel)
 
 # ax.set(xlim=(0, 100), xticks=np.arange(1, 8),
 #        ylim=(0, 10), yticks=np.arange(1, 8))
+
+ax.set_xlabel('Package complexity')
+ax.set_ylabel('Package size')
+ax.set_title('Alpine packages over time')
+ax.legend()
+# ax.text(75, .025, r'$\mu=115,\ \sigma=15$')
+# ax.axis([55, 175, 0, 0.03])
+ax.grid(True)
 
 imgpath = __file__.replace('.py', '.png')
 plt.savefig(imgpath)
