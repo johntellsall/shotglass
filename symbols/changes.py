1aab62a86baf45c097546bf9370a0b0540579815
John Mitchell <johnlmitchell@gmail.com>
2025-06-08 09:27:47 -0700

    sample tags

--- symbols/changes.py
+++ symbols/changes.py
@@ -1,13 +1,25 @@
 # changes.py -- show file changes over time
 # - Dnsmasq, 20 samples over 90 releases
 
-PROJ = '../SOURCE/dnsmasq'
-
+import re
+import sys
 import run
 
-def main():
-    tags = run.git_tag_list(PROJ)
+def is_tag_interesting(tag):
+    return re.fullmatch('v[0-9.]+', tag) is not None
+
+def sample_tags(proj):
+    tags = run.git_tag_list(proj)
+    tags = [tag for tag in tags if is_tag_interesting(tag)]
+    # Pick every tenth tag
+    sampled_tags = tags[::10] # FIXME:
+    return sampled_tags
+
+def main(paths):
+    proj = paths[0]
+    tags = sample_tags(proj)
     print(tags)
+   
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1:])
