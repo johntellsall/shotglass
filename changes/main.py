08fc3b039665fbbba91880ed84b0974af92ee578
John Mitchell <johnlmitchell@gmail.com>
2025-07-04 11:44:54 -0700

    wip squarify

--- changes/main.py
+++ changes/main.py
@@ -1,6 +1,7 @@
 from symbols import parse_path
 
 import click
+import squarify
 from PIL import Image
 import math
 import subprocess
@@ -25,17 +26,29 @@ def is_interesting(sourcepath):
 @click.argument('files', nargs=-1, type=click.Path(exists=True))
 def main(files):
     """Print contents of each file passed as argument."""
+    width, height = 800, 600
 
     files = [file for file in files if is_interesting(file)]
-    for file_path in files:
-        symbols = parse_path(file_path)
-        img = render(symbols)
-        if img is None:
-            continue
-        with tempfile.NamedTemporaryFile(suffix=".png", delete=True) as tmp:
-            img.save(tmp.name)
-            print(f'{symbols.path}: {symbols.num_lines} lines, {len(symbols.symbols)} symbols, size={img.size}')
-            show(tmp.name)
+    symfiles = [parse_path(file_) for file_ in files]
+    symfiles = [s for s in symfiles if s.num_lines >= 10]
+    symfiles.sort(key=lambda s: s.num_lines, reverse=True)
+
+    sizes = [s.num_lines for s in symfiles]
+    sizes = squarify.normalize_sizes(sizes, width, height)
+    rects = squarify.padded_squarify(sizes, 0, 0, width, height)
+    print(rects)
+
+
+
+    # for file_path in files:
+    #     symbols = parse_path(file_path)
+    #     img = render(symbols)
+    #     if img is None:
+    #         continue
+    #     with tempfile.NamedTemporaryFile(suffix=".png", delete=True) as tmp:
+    #         img.save(tmp.name)
+    #         print(f'{symbols.path}: {symbols.num_lines} lines, {len(symbols.symbols)} symbols, size={img.size}')
+    #         show(tmp.name)
 
 if __name__ == '__main__':
     main()
