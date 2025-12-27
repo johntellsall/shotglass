8255788e1de483018cf7d1a389a300bae18a63c2
John Mitchell <johnlmitchell@gmail.com>
2025-12-27 15:45:44 -0800

    test path

--- symbols/test_db.py
+++ symbols/test_db.py
@@ -1,14 +1,33 @@
+import pytest
+
 import main
 from state import queryall
 
-def test_raw_add_project():
+
+@pytest.fixture(scope="module")
+def test_con():
     con = main.raw_add_project(
         "../SOURCE/flask", is_testing=True, only_interesting=True
     )
-    release_rows = queryall(con, sql='select * from release')
+    return con
+
+def test_release(test_con):
+    release_rows = queryall(test_con, sql='select * from release')
     releases = [dict(row) for row in release_rows]
     assert len(releases) >= 1
     assert releases[0]["label"] == "upstream/3.1.2"
+
+
+def test_path(test_con):
+    file_rows = queryall(test_con, sql='select * from file')
+    files = [dict(row) for row in file_rows]
+    assert len(files) >= 1
+    myfile = files[0]
+    assert myfile['path'] == 'src/flask/__init__.py'
+    assert myfile['release'] == 'upstream/3.1.2'
+    assert int(myfile['size_bytes']) > 1000
+
+
     # file_rows = queryall(con, sql='select * from file where path = "src/flask/app.py"')
     # item = dict(file_rows[0])
     # print('FILE:', item)
